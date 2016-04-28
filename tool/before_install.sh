3f2ff013ffc3110b1855f2426538774170708a6f
Seth Ladd <sethladd@google.com>
2016-04-28 10:29:08 -0700

    put flutter's dart on the path

--- tool/before_install.sh
+++ tool/before_install.sh
@@ -3,4 +3,8 @@
 # Fast fail the script on failures.
 set -e
 
+# Use the version of Dart SDK from the Flutter repository instead of whatever
+# version is in the PATH.
+export PATH="`pwd`/../flutter/bin/cache/dart-sdk/bin:$PATH"
+
 (cd ..; git clone https://github.com/flutter/flutter.git -b alpha; cd flutter; ./bin/flutter doctor ; pub get)
