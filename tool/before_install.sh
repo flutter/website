e36e25b8b01c9b73422793bc94671bde5dc283e9
Seth Ladd <sethladd@google.com>
2016-04-28 10:47:45 -0700

    get path right

--- tool/before_install.sh
+++ tool/before_install.sh
@@ -7,4 +7,4 @@ echo "Downloading Flutter"
 
 # Run doctor to download the Dart SDK that is vendored with Flutter
 
-(cd ..; git clone https://github.com/flutter/flutter.git -b alpha ; cd flutter ; flutter doctor)
+(cd ..; git clone https://github.com/flutter/flutter.git -b alpha ; cd flutter ; ./bin/flutter doctor)
