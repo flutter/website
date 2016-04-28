6bcb3af6cf9ce419280c4cc12a5cd77fe220006a
Seth Ladd <sethladd@google.com>
2016-04-28 09:56:01 -0700

    default jekyll build location, default to staging server on non-master merges, add firebase rules

--- tool/travis.sh
+++ tool/travis.sh
@@ -19,13 +19,16 @@ gem install bundler
 bundle install
 
 echo "Building site."
-bundle exec jekyll build -d build
+bundle exec jekyll build
+
+# TODO: deploy to a personal staging site, based on github ID, when not
+#       merging into master
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Deploying to Firebase."
 
     npm install -g firebase-tools
-    firebase deploy --token "$FIREBASE_TOKEN"
+    firebase deploy --token "$FIREBASE_TOKEN" -f sweltering-fire-2088
   fi
 fi
