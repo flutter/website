24c608a98e3f2383e7df0482cd82ab9353cdb0c8
Shams Zakhour <shaza@google.com>
2017-02-15 10:44:54 -0800

    Commenting out an unused import.

--- _includes/_code/container/main.dart
+++ _includes/_code/container/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+//import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
   //debugPaintSizeEnabled = true;
