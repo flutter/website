3d5cf690163f8fb8c0593562163932d9976870d1
Faisal Abid <faisal.abid@gmail.com>
2017-03-13 12:56:05 -0400

    comment out stager (#435)

--- tool/travis.sh
+++ tool/travis.sh
@@ -83,15 +83,16 @@ if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   firebase -P sweltering-fire-2088 --token "$FIREBASE_TOKEN" deploy
 fi
 
-if [ "$BRANCH" != "master" ]; then
-    echo "deploying to stage environment"
-    echo $FIREBASE_FILE >> ./service_account.json
-    export FIREBASE_AUTH=`oauth2l fetch --json ./service_account.json firebase.database userinfo.email 2>&1`
-    cd tool/
-    ../../flutter/bin/cache/dart-sdk/bin/pub get
-    export PROJECT_NAME=`../../flutter/bin/cache/dart-sdk/bin/dart prdeployer.dart $BRANCH $FIREBASE_AUTH 2>&1`
-    cd ../
-    echo "Deploying to $PROJECT_NAME"
-    npm install --global firebase-tools@3.0.0
-    firebase -P "$PROJECT_NAME" --token "$FIREBASE_TOKEN_DEV" deploy
-fi
+
+#if [ "$BRANCH" != "master" ]; then
+#    echo "deploying to stage environment"
+#    echo $FIREBASE_FILE >> ./service_account.json
+#    export FIREBASE_AUTH=`oauth2l fetch --json ./service_account.json firebase.database userinfo.email 2>&1`
+#    cd tool/
+#    ../../flutter/bin/cache/dart-sdk/bin/pub get
+#    export PROJECT_NAME=`../../flutter/bin/cache/dart-sdk/bin/dart prdeployer.dart $BRANCH $FIREBASE_AUTH 2>&1`
+#    cd ../
+#    echo "Deploying to $PROJECT_NAME"
+#    npm install --global firebase-tools@3.0.0
+#    firebase -P "$PROJECT_NAME" --token "$FIREBASE_TOKEN_DEV" deploy
+#fi
