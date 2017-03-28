1f264cdc097ad7f17e286f852b07350566fb807e
Seth Ladd <sethladd@google.com>
2017-03-28 13:05:01 -0700

    remove wacky JS fixing sidenav scrolling

--- js/customscripts.js
+++ js/customscripts.js
@@ -1,15 +1,6 @@
 $( document ).ready(function() {
-
-    // this script says, if the height of the viewport is greater than 800px,
-    // then insert affix class, which makes the nav bar float in a fixed
-    // position as your scroll. if you have a lot of nav items, this height may
-    // not work for you.
-    var h = $(window).height();
-    //console.log (h);
-    if (h > 800) {
-        $( "#mysidebar" ).attr("class", "nav affix");
-    }
-    // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
+    // activate tooltips. although this is a bootstrap js function,
+    // it must be activated this way in your theme.
     $('[data-toggle="tooltip"]').tooltip({
         placement : 'top'
     });
