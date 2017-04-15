1cb8144607c2fd171242e88e74f25e9a4c766014
Chris Bracken <chris@bracken.jp>
2017-04-15 10:04:57 -0700

    DefaultTextStyle.merge is now a static method
    In flutter/flutter#9358, DefaultTextStyle.merge was migrated from a
    constructor to a static.
    
    Announcement sent to flutter-dev:
    https://groups.google.com/forum/#!topic/flutter-dev/0GFDGRYKGro

--- _includes/_code/pavlova/main.dart
+++ _includes/_code/pavlova/main.dart
@@ -101,8 +101,7 @@ Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlo
 
     // DefaultTextStyle.merge allows you to create a default text
     // style that is inherited by its child and all subsequent children.
-    var iconList = new DefaultTextStyle.merge(
-      context: context,
+    var iconList = DefaultTextStyle.merge(
       style: descTextStyle,
       child: new Container(
         padding: new EdgeInsets.all(20.0),
