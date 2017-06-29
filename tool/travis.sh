03a3dd71f624cd71c94d55cacd727d0d54e84bb3
Michael Thomsen <mit-mit@users.noreply.github.com>
2017-06-29 16:37:18 +0200

    Update firebase tool invocations to be non-interactive

--- tool/travis.sh
+++ tool/travis.sh
@@ -80,7 +80,7 @@ if [ "$TRAVIS_EVENT_TYPE" = "push" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
   # Deploy pushes to master to Firebase hosting.
   echo "Deploying to Firebase."
   npm install --global firebase-tools@3.0.0
-  firebase -P sweltering-fire-2088 --token "$FIREBASE_TOKEN" deploy
+  firebase -P sweltering-fire-2088 --token "$FIREBASE_TOKEN" --non-interactive deploy
 fi
 
 
@@ -95,6 +95,6 @@ if [ "$ENABLE_PR_BOT" = "true" ]; then
         cd ../
         echo "Deploying to $PROJECT_NAME"
         npm install --global firebase-tools@3.0.0
-        firebase -P "$PROJECT_NAME" --token "$FIREBASE_TOKEN_DEV" deploy
+        firebase -P "$PROJECT_NAME" --token "$FIREBASE_TOKEN_DEV" --non-interactive deploy
     fi
 fi
