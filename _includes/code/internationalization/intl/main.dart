f73e59dda6c818e2255ba031c94d2d264f613ab5
Devon Carew <devoncarew@google.com>
2018-06-01 10:22:47 -0700

    adjust analysis options (#1050)

--- _includes/code/internationalization/intl/main.dart
+++ _includes/code/internationalization/intl/main.dart
@@ -48,7 +48,7 @@ class DemoLocalizations {
     final String name = locale.countryCode.isEmpty ? locale.languageCode : locale.toString();
     final String localeName = Intl.canonicalizedLocale(name);
 
-    return initializeMessages(localeName).then((Null _) {
+    return initializeMessages(localeName).then((_) {
       Intl.defaultLocale = localeName;
       return new DemoLocalizations();
     });
