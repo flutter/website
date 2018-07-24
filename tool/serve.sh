4499876e5605ce7fce238b7134585043fcc97307
Patrice Chalin <pchalin@gmail.com>
2018-07-24 15:40:49 -0400

    chore: use configurable server.sh script
    Get the local server port from the `_config.yml` file.
    Choosing 4002 to avoid clashing with site-www and site-webdev.

--- tool/serve.sh
+++ tool/serve.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+#
+# Copied from https://github.com/dart-lang/site-shared/blob/master/tool/serve.sh
 
 set -e -o pipefail
 
+# Assume that this script will be linked to ~/scripts/foo or ~/tool/foo in the parent repo.
 cd `dirname $0`/..
 
 if [[ "$1" == "--dev" && -e _config_dev.yml ]]; then
@@ -18,9 +21,11 @@ if [[ -n $CONFIG ]]; then
   CONFIG="--config _config.yml$CONFIG"
 fi
 
+PORT=$(grep '^port:' _config.yml | awk '{ print $2}' || echo 4000)
+
 bundle exec jekyll build $CONFIG --incremental --watch &
 j_pid=$!
-superstatic --port 4002 &
+superstatic --port $PORT &
 f_pid=$!
 echo "cached PIDs: $j_pid, $f_pid"
 trap "{ kill $j_pid; kill $f_pid; exit 0;}" SIGINT
