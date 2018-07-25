c3c8298d9fe2ca26f2fc4396940072b772c33258
Patrice Chalin <chalin@users.noreply.github.com>
2018-07-25 09:10:15 -0400

    chore: upgrade Jekyll prettifier plugin (#1113)
    Copied from https://github.com/dart-lang/site-shared/blob/master/src/_plugins/prettify.rb

--- _plugins/prettify.rb
+++ _plugins/prettify.rb
@@ -1,77 +1,117 @@
+# Copied from https://github.com/dart-lang/site-shared/blob/master/src/_plugins/prettify.rb
+# and slightly adapted for use with prism.js
+#
 # Copyright (c) 2012, the Dart project authors.  Please see the AUTHORS file
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
 require 'cgi'
+require 'liquid/tag/parser' # https://github.com/envygeeks/liquid-tag-parser
 
 module Prettify
 
   # Wraps code with tags for Prettify.
   #
-  # Example usage:
-  # 
-  # {% prettify dart %}
-  # // dart code here
-  # {% endprettify %}
+  # Arguments:
   #
-  # The language name can be ommitted if it is not known.
+  # - The first unnamed optional argument is the prettifier lang argument.
+  #   Use 'nocode' or 'none' as the language to turn of prettifying.
+  # - class="..."
+  # - tag="pre|pre+code|code|code+br". The HTML element used to wrap the prettified code.
+  #   Default is pre. The `code` element is used for `code+br`; in addition,
+  #   newlines in the code excerpt are reformatted at `<br>` elements.
+  #
+  # Example usage:
+  #
+  # {% prettify dart %}
+  # var hello = 'world';
+  # {% endprettify %}
+
   class Tag < Liquid::Block
 
-    Syntax = /\s*(\w+)\s*/o
-
-    def initialize(tag_name, markup, tokens)
+    def initialize(tag_name, stringOfArgs, tokens)
       super
-      if markup =~ Syntax
-        @lang = $1
-      end
+      @args = Liquid::Tag::Parser.new(stringOfArgs).args
+      @lang = @args[:argv1]
+      @tag = @args[:tag] || 'pre'
+      @tag = 'code' if @tag == 'code+br';
+      initCssClasses
     end
 
-    def unindent(original)
-      indent = original.split("\n").select do |line|
-        !line.strip.empty?
-      end.map do |line|
-        line.index(/[^\s]/)
-      end.compact.min || 0
-      original.gsub(/^[[:blank:]]{#{indent}}/, '')
+    def initCssClasses
+      @cssClasses = []
+      unless @lang == 'nocode' || @lang == 'none'
+        @cssClasses << 'prettyprint highlighter-rouge highlight' # TODO: Unnecessary, remove after transition
+        @cssClasses << "lang-#{@lang}" if @lang
+        @cssClasses << "language-#{@lang}" if @lang # TODO: Unnecessary, remove after transition
+      end
+      @cssClasses << @args[:class] if @args[:class]
+    end
+
+    def classAttr
+      @cssClasses.empty? ? '' : " class=\"#{@cssClasses.join(' ')}\""
     end
 
     def render(context)
-      # out = '<pre class="prettyprint linenums'
-      out = '<pre class="prettyprint highlighter-rouge highlight'
-      unless @lang.nil?
-        out += ' lang-' + @lang
-        out += ' language-' + @lang
+      out = "<#{@tag}#{classAttr}>"
+      out += '<code>' if @tag == 'pre'
+
+      code = trimMinLeadingSpace(super)
+      # Strip leading and trailing whitespace so that <pre> and </pre> tags wrap tightly
+      code.strip!
+      code = CGI::escapeHTML(code)
+
+      if @args[:tag] == 'code+br'
+        code.gsub!(/\n[ \t]*/) { |s|
+          "<br>\n#{'&nbsp;' * (s.length - 1)}"
+        }
       end
-      out += '"><code>'
 
-      contents = unindent(super)
+      # Names of tags previously supported: highlight, note, red, strike.
+      code.gsub!(/\[\[(\w+)\]\]/, '<span class="nocode \1">') # TODO: 'nocode' is unnecessary, remove after transition
+      code.gsub!(/\[\[\/(\w+)\]\]/, '</span>')
 
-      contents = contents.strip
-      contents = CGI::escapeHTML(contents)
+      # Flutter tag syntax variant:
+      code.gsub!(/\/\*\*(\w+)\*\//, '<span class="nocode \1">') # TODO: 'nocode' is unnecessary, remove after transition
+      code.gsub!(/\/\*-(\w+)\*\//, '</span>')
 
-      contents.gsub!('[[strike]]', '<span class="nocode strike">')
-      contents.gsub!('[[/strike]]', '</span>')
-      contents.gsub!('/**strike*/', '<span class="nocode strike">')
-      contents.gsub!('/*-strike*/', '</span>')
+      code.gsub!('[!', '<span class="highlight">')
+      code.gsub!('!]', '</span>')
 
-      contents.gsub!('[[highlight]]', '<span class="nocode highlight">')
-      contents.gsub!('[[/highlight]]', '</span>')
-      contents.gsub!('/**highlight*/', '<span class="nocode highlight">')
-      contents.gsub!('/*-highlight*/', '</span>')
-
-      contents.gsub!('[[note]]', '<span class="nocode note">')
-      contents.gsub!('[[/note]]', '</span>')
-      contents.gsub!('/**note*/', '<span class="nocode note">')
-      contents.gsub!('/*-note*/', '</span>')
-
-      contents.gsub!('[[red]]', '<span class="nocode red">')
-      contents.gsub!('[[/red]]', '</span>')
-      contents.gsub!('/**red*/', '<span class="nocode red">')
-      contents.gsub!('/*-red*/', '</span>')
-
-      out += contents + "</code></pre>"
+      out += code
+      out += '</code>' if @tag == 'pre'
+      out += "</#{@tag}>"
     end
 
+    def trimMinLeadingSpace(code)
+      lines = code.split(/\n/);
+
+      # 1. Trim leading blank lines
+      while lines.first =~ /^\s*$/ do lines.shift; end
+
+      # 2. Trim trailing blank lines. Also determine minimal
+      # indentation for the entire code block.
+      # This is required for uses of prettify that are indented in
+      # markdown -- for example, when used in a list.
+
+      # Last line should consist of the indentation of the `endprettify`
+      # (when it is on a separate line).
+      last_line = lines.last =~ /^\s*$/ ? lines.pop : ''
+      while lines.last =~ /^\s*$/ do lines.pop end
+      min_len = last_line.length
+
+      nonblanklines = lines.reject { |s| s.match(/^\s*$/) }
+
+      # 3. Determine length of leading spaces to be trimmed
+      len = nonblanklines.map{ |s|
+        matches = s.match(/^[ \t]*/)
+        matches ? matches[0].length : 0 }.min
+
+      # Only trim the excess relative to min_len
+      len = len < min_len ? min_len : len - min_len
+
+      len == 0 ? code : lines.map{|s| s.length < len ? s : s.sub(/^[ \t]{#{len}}/, '')}.join("\n")
+    end
   end
 end
 
