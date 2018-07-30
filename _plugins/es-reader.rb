17df109aea12849ea66683ec307bde657f6d6f40
twinber <twinber@hotmail.com>
2018-07-30 19:36:06 +0200

    prueba sustitucion 404.md

--- _plugins/es-reader.rb
+++ _plugins/es-reader.rb
@@ -5,7 +5,6 @@ module Jekyll
       @site.layouts = LayoutReader.new(site).read
       read_directories
       sort_files!
-      puts(site.config)
       @site.data = Jekyll::EsDataReader.new(site).read(site.config["data_dir"])
       CollectionReader.new(site).read
       ThemeAssetsReader.new(site).read
