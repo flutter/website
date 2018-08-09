3412debb9871e23555708d3aa6da53476238275a
Greg Spencer <gspencergoog@users.noreply.github.com>
2018-08-09 13:38:50 -0700

    Move continuous integration to Cirrus from Travis (#1138)
    This moves the CI setup to use Cirrus CI instead of Travis, to be in line with the other Flutter repos.
    
    I've renamed some of the scripts so that they aren't Travis specific, and updated their bash to be more idiomatic and safe for filenames with spaces.
    
    I've moved most of the install into a Docker image that can be updated with the tool/docker_linux/docker_build.sh and tool/docker_linux/docker_build.sh scripts. This includes the Ruby gems bundles, so the Gemfile and Gemfile.lock have moved to tool/docker_linux as well.
    
    One unrelated change I had to make was to update two external URLs that were returning 404s in bootstrap-into-dart.md and widgets-intro.md. The build wouldn't pass without this change.
    
    With the Docker image configuration, PR checks now take two and a half minutes to complete.

---
+++ tool/build_check_deploy.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+# Fast fail the script on failures.
+set -e
+
+# Write errors to stderr
+function error() {
+  echo "FAIL: $@" 1>&2
+}
+
+# Checks the formatting of the Dart files to make sure it conforms to dartfmt
+# output.
+function check_formatting() {
+  # Read files one line at a time, in case there are spaces in the names.
+  local fmt_result=()
+  IFS=$'\n' fmt_result=($("$dartfmt" -n "$@" 2>&1))
+  local fmt_code=$?
+  if [[ "$fmt_code" != 0 ]]; then
+    error "dartfmt exited with the following exit code: ${fmt_code}"
+    error "${fmt_result[@]}"
+    exit 1
+  elif [[ "${#fmt_result[@]}" == 0 ]]; then
+    echo "No formatting errors!"
+  else
+    error "There are formatting errors in the following files:"$'\n'
+    for file in "${fmt_result[@]}"; do
+      error "===== $file ====="
+      cp "$file" "$file.expected"
+      "$dartfmt" -w "$file.expected" &> /dev/null
+      error "----- expected format -----"
+      cat "$file.expected" 1>&2
+      error "----- current format -----"
+      cat "$file" 1>&2
+      error "----- diff -----"
+      diff "$file.expected" "$file" 1>&2
+      error "===== /end $file ====="$'\n'
+      rm "$file.expected"
+    done
+    return 1
+  fi
+}
+
+# Deploys website to Firebase, trying up to total_tries times.
+function deploy() {
+  local total_tries="$1"
+  local remaining_tries=$(($total_tries - 1))
+  local project="$2"
+  while [[ "$remaining_tries" > 0 ]]; do
+    # FIREBASE_TOKEN is set in the .cirrus.yml file.
+    firebase deploy --token "$FIREBASE_TOKEN" --non-interactive --project "$project" && break
+    remaining_tries=$(($remaining_tries - 1))
+    error "Error: Unable to deploy project $project to Firebase. Retrying in five seconds... ($remaining_tries tries left)"
+    sleep 5
+  done
+
+  [[ "$remaining_tries" == 0 ]] && {
+    error "Deployment still failed after $total_tries tries: '$@'"
+    return 1
+  }
+  return 0
+}
+
+# Used to reliably find the absolute location of this script.
+function script_location() {
+  local script_location="${BASH_SOURCE[0]}"
+  # Resolve symlinks
+  while [[ -h "$script_location" ]]; do
+    DIR="$(cd -P "$(dirname "$script_location")" >/dev/null && pwd)"
+    script_location="$(readlink "$script_location")"
+    [[ "$script_location" != /* ]] && script_location="$DIR/$script_location"
+  done
+  echo "$(cd -P "$(dirname "$script_location")" >/dev/null && pwd)"
+}
+
+# Set the FLUTTER_ROOT and PATH, in case we're running this script locally.
+# Assumes that the flutter dir is at the same level as the website dir.
+FLUTTER_ROOT="${FLUTTER_ROOT:-$(script_location)/../../flutter}"
+export PATH="$FLUTTER_ROOT/bin/cache/dart-sdk/bin:$FLUTTER_ROOT/bin:$PATH"
+
+if [[ ! -x "$FLUTTER_ROOT/bin/flutter" ]]; then
+  error "The FLUTTER_ROOT variable must point to your Flutter repo for this "
+  error "script to run correctly. Alternatively, it can be located as a sibling"
+  error "to the 'website' directory."
+  error "Currently looking for FLUTTER_ROOT in '$FLUTTER_ROOT'"
+  exit 1
+fi
+
+# Use the version of Dart SDK from the Flutter repository instead of whatever
+# version is in the PATH. FLUTTER_ROOT comes from the Cirrus Docker image.
+# This is mainly guarding against something installing Dart (e.g. as a
+# dependency) in another location and somehow getting into the PATH ahead of the
+# one in FLUTTER_ROOT, or against a corrupt install of the SDK. It's unlikely,
+# but we want to fail to find these if it happens.
+dart="$FLUTTER_ROOT/bin/cache/dart-sdk/bin/dart"
+dartfmt="$FLUTTER_ROOT/bin/cache/dart-sdk/bin/dartfmt"
+pub="$FLUTTER_ROOT/bin/cache/dart-sdk/bin/pub"
+flutter="$FLUTTER_ROOT/bin/flutter"
+
+echo "Using Dart version:"
+"$dart" --version
+
+"$flutter" packages get
+
+# Analyze the stand-alone sample code files
+for sample in _includes/code/*/*; do
+  if [[ -d "${sample}" ]]; then
+    echo "Run flutter packages get on ${sample}"
+    "$flutter" packages get ${sample}
+  fi
+done
+
+echo "Run flutter analyze on _includes/code/"
+"$flutter" analyze --no-current-package _includes/code/
+
+echo "Extract Dart snippets from the markdown documentation."
+"$dart" tool/extract.dart
+
+echo "Analyzing the extracted Dart libraries."
+"$flutter" analyze --no-current-package example/
+
+echo "Check formatting of the extracted Dart libraries."
+check_formatting example/*.dart
+
+echo "Building site."
+bundle exec jekyll build
+
+echo "Validating all links."
+rake checklinks
+echo "SUCCESS: All integration checks complete."
+
+# Deploy on all non-PR master branch builds.
+if [[ -z "$CIRRUS_PR" && "$CIRRUS_BRANCH" == "master" ]]; then
+  echo "Deploying website to Firebase."
+  deploy 5 sweltering-fire-2088
+  echo "SUCCESS: Website deployed successfully."
+fi
+
+exit 0
