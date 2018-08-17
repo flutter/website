a87868808099d69f54acc839d2f5e8eb8e276d97
Patrice Chalin <pchalin@gmail.com>
2018-08-17 17:51:05 -0400

    chore: tweak config and update setup instructions

--- tool/serve.sh
+++ tool/serve.sh
@@ -7,15 +7,29 @@ set -e -o pipefail
 # Assume that this script will be linked to ~/scripts/foo or ~/tool/foo in the parent repo.
 cd `dirname $0`/..
 
-if [[ "$1" == "--dev" && -e _config_dev.yml ]]; then
-  shift;
-  CONFIG=",_config_dev.yml$CONFIG"
-fi
+SERVE=superstatic
 
-if [[ "$1" == "--pin-now" && -e _config_now.yml ]]; then
-  shift;
-  CONFIG=",_config_now.yml$CONFIG"
-fi
+while [[ "$1" == -* ]]; do
+  case "$1" in
+    --dev)        FILE=_config_dev.yml
+                  if [[ -e $FILE ]]; then
+                    CONFIG=",$FILE$CONFIG"
+                  else
+                    echo "Warning: $1 option ignored because $FILE not found"
+                  fi
+                  shift;;
+    --pin-now)    FILE=_config_now.yml
+                  if [[ -e $FILE ]]; then
+                    CONFIG=",$FILE$CONFIG"
+                  else
+                    echo "Warning: $1 option ignored because $FILE not found"
+                  fi
+                  shift;;
+    --firebase)   SERVE="firebase serve"; shift;;
+    -h|--help)    echo "Usage: $(basename $0) [--dev|--pin-now] [--firebase] [--help]"; exit 0;;
+    *)            echo "ERROR: Unrecognized option: $1. Use --help for details."; exit 1;;
+  esac
+done
 
 if [[ -n $CONFIG ]]; then
   CONFIG="--config _config.yml$CONFIG"
@@ -25,7 +39,7 @@ PORT=$(grep '^port:' _config.yml | awk '{ print $2}' || echo 4000)
 
 bundle exec jekyll build $CONFIG --incremental --watch &
 j_pid=$!
-superstatic --port $PORT &
+$SERVE --port $PORT &
 f_pid=$!
 echo "cached PIDs: $j_pid, $f_pid"
 trap "{ kill $j_pid; kill $f_pid; exit 0;}" SIGINT
