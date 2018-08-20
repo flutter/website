4ed3f9f42719742a0f13f3d3c9ce1654a197bd51
Patrice Chalin <chalin@users.noreply.github.com>
2018-08-20 08:00:22 -0400

    Site file reorg: move site sources to src folder (#1169)
    * Move site sources under src
    
    * Adjust .gitignore
    
    * Setup basic .firebaserc
    
    * firebase.json: cleanup formatting (no content change)
    
    * Code excerpt from markdown is now under example.g

--- tool/extract.dart
+++ tool/extract.dart
@@ -3,10 +3,13 @@ import 'dart:math';
 
 import 'package:path/path.dart' as p;
 
+const siteSrcPath = 'src';
+const generatedExampleDirPath = 'example.g';
+
 /// Extract Dart snippets from the markdown documentation.
 void main(List<String> args) {
   // Validate our cwd.
-  if (!new File('pubspec.yaml').existsSync()) {
+  if (!File('pubspec.yaml').existsSync()) {
     print('This tool must be run from the project root.');
     exit(1);
   }
@@ -16,9 +19,12 @@ void main(List<String> args) {
 
   // Traverse all markdown files in the repository.
   int extractCount = 0;
-  Iterable<FileSystemEntity> files = Directory.current
+  Iterable<FileSystemEntity> files = Directory(siteSrcPath)
       .listSync(recursive: true)
-      .where((FileSystemEntity entity) => entity is File && entity.path.endsWith('.md') && !entity.path.contains('README.md'));
+      .where((FileSystemEntity entity) =>
+          entity is File &&
+          entity.path.endsWith('.md') &&
+          !entity.path.contains('README.md'));
   files.forEach((FileSystemEntity file) => extractCount += _processFile(file));
   print('\n$extractCount code snippets extracted.');
 }
@@ -38,7 +44,8 @@ int _processFile(File file) {
 
   while (index < lines.length) {
     // Look for ```dart sections.
-    if (lines[index].trim().startsWith('```dart') && lastComment?.trim() != 'skip') {
+    if (lines[index].trim().startsWith('```dart') &&
+        lastComment?.trim() != 'skip') {
       int startIndex = index + 1;
       index++;
       while (index < lines.length && !lines[index].trim().startsWith('```')) {
@@ -71,27 +78,23 @@ int _processFile(File file) {
   return count;
 }
 
-void _extractSnippet(String filename, int snippet, int startLine, Iterable<String> lines,
+void _extractSnippet(
+    String filename, int snippet, int startLine, Iterable<String> lines,
     {String includeSource}) {
-  bool hasImport = lines.any((String line) => line.trim().startsWith('import '));
-  String path = 'example/${filename.replaceAll('-', '_').replaceAll('.', '_')}_'
-      '$snippet.dart';
+  bool hasImport =
+      lines.any((String line) => line.trim().startsWith('import '));
+  String path = p.join(generatedExampleDirPath,
+      '${filename.replaceAll('-', '_').replaceAll('.', '_')}_$snippet.dart');
 
   String source = '// Extracted from $filename, line $startLine.\n';
 
-  if (!hasImport) {
-    source += "import 'package:flutter/material.dart';\n\n";
-  }
-
-  if (includeSource != null) {
-    source += "$includeSource\n";
-  }
+  if (!hasImport) source += "import 'package:flutter/material.dart';\n\n";
+  if (includeSource != null) source += "$includeSource\n";
 
   source += '${lines.join('\n')}\n';
-
   source = _removeMarkup(source);
 
-  new File(path).writeAsStringSync(source);
+  File(path).writeAsStringSync(source);
   print('  ${lines.length} line snippet ==> $path');
 }
 
@@ -106,7 +109,7 @@ String _removeMarkup(String source) {
 }
 
 void clean() {
-  var exampleDir = new Directory('example');
+  var exampleDir = Directory(generatedExampleDirPath);
   if (!exampleDir.existsSync()) {
     exampleDir.createSync();
   }
@@ -116,8 +119,8 @@ void clean() {
   files.forEach((file) => file.deleteSync());
 }
 
-final blankLine = new RegExp(r'^\s*$');
-final _leadingWhitespace = new RegExp(r'^[ \t]*');
+final blankLine = RegExp(r'^\s*$');
+final _leadingWhitespace = RegExp(r'^[ \t]*');
 
 /// Unindent [lines] to the extent possible without losing
 /// the relative inter-line indentation. Note that blank
