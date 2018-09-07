73acc38015d4e32885c3abfde3053e428918281a
Patrice Chalin <pchalin@gmail.com>
2018-09-06 22:10:59 -0400

    [dash] A little help for zsh users, and ensure bundle install is always run

--- tool/install.sh
+++ tool/install.sh
@@ -2,6 +2,8 @@
 
 set -e -o pipefail
 
+[[ -z "$DART_SITE_ENV_DEFS" ]] && . ./tool/env-set.sh
+
 cd `dirname $0`/..
 
 if [[ "$(node --version)" < "v10" ]]; then
@@ -13,11 +15,13 @@ else
     echo "Node version: $(node --version)"
 fi
 
+if ! __type_t travis_fold; then travis_fold() { true; } fi # For ZSH users
+
 travis_fold start install.npm_install
   (set -x; npm install --no-optional)
 travis_fold end install.npm_install
 
-if [[ "$TASK" == build* ]]; then
+if [[ -z "$TRAVIS" || "$TASK" == *build* ]]; then
   travis_fold start install.bundle
     (set -x; bundle install)
   travis_fold end install.bundle
