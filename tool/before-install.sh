131414aaad84f44d559af08b1877ba9e819150c2
Patrice Chalin <pchalin@gmail.com>
2018-09-10 15:12:22 -0400

    Set FLUTTER_ROOT in env-set.sh

--- tool/before-install.sh
+++ tool/before-install.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-FLUTTER_ROOT=flutter
-FLUTTER_BIN="$FLUTTER_ROOT/bin"
-
-if [[ ! -e "$FLUTTER_ROOT" ]]; then
-  echo "ERROR: The ~/flutter git submodule is missing. For initialization instructions, see the repo README."
-  exit 1;
-fi
-
 source tool/shared/before-install.sh
+
+# Site-specific settings here: currently, none.
 
 travis_fold start before_install.flutter
 echo "Refreshing Flutter repo and running doctor:"
