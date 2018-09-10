1c196037d66cdf0c7ace9b37ad48a0913c6156d9
Patrice Chalin <chalin@users.noreply.github.com>
2018-09-10 12:10:00 -0400

    Setup use of site-shared, drop local copies of setup files (#1256)
    * Setup use of site-shared, drop local copies of setup files
    
    * Final synlink rename

---
+++ tool/before-install.sh
@@ -0,0 +1,1 @@
+shared/before-install.sh
--- tool/env-set.sh
+++ tool/env-set.sh
@@ -1,18 +1,5 @@
-# This bash file is meant to be source'd, not executed.
+# This bash file is meant to be source'd from the repo root, not executed.
 
 source tool/shared/env-set.sh
 
-# Temporary, until we start using site-shared:
-
-# nvm install 10
-# nvm use 10
-# rvm install 2.4.3
-# rvm use 2.4.3
-
-# function travis_fold () { true; }
-# export -f travis_fold
-
-# : ${TMP:=tmp}
-# [[ -e "$TMP" ]] || mkdir -pv "$TMP"
-
-# Site specific settings here: currently, none.
+# Site-specific settings here: currently, none.
