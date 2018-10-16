cfc853b8811af3f108ac9b58f9e1ed1407a80da9
Fernando Toledo <fguerrero@google.com>
2018-10-16 16:01:56 -0700

    removes demo ID

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -86,7 +86,6 @@ function initVideoModal() {
   if (videoModalObject.length) {
     // there is a video modal in the DOM, load the YouTube API
     var tag = document.createElement('script');
-    tag.id = 'iframe-demo';
     tag.src = 'https://www.youtube.com/iframe_api';
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
