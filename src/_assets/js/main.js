4838bc8f664abfb541d1d025695ed64ed99330b1
István Soós <isoos@users.noreply.github.com>
2019-04-03 01:53:36 +0200

    Override table-of-content HTML in the template. (#2562)

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -40,18 +40,6 @@ function adjustToc() {
     $('html, body').animate({ scrollTop: 0 }, 'fast');
   })
 
-  // TODO: consider doing most of the HTML TOC manipulation statically (maybe requiring a jekyll-toc adaptor plugin)
-  // Bootstrap 4's ScrollSpy works only for .nav or .list-group,
-  // with items and link classes set too. Add the classes.
-  var toc = $(tocWrapper).find('ul.section-nav');
-  $(toc).addClass('nav');
-
-  var ul = $(toc).find('ul');
-  $(ul).addClass('nav');
-  var li = $(toc).find('.toc-entry');
-  $(li).addClass('nav-item');
-  $(li).find('a').addClass('nav-link');
-
   $('body').scrollspy({ offset: 100, target: tocId });
 }
 
