c197f045fa0ed2a80055c1cbc238579ccf77b01b
Jon Tippens <jtippens@google.com>
2019-10-17 12:08:57 -0400

    Remove comment

--- src/_packages/dartpad_picker/web/dartpad_picker_main.dart
+++ src/_packages/dartpad_picker/web/dartpad_picker_main.dart
@@ -7,7 +7,6 @@ import 'package:dartpad_picker/dartpad_picker.dart';
 
 void main() {
   if (isMobile()) {
-    // querySelector('.dash-dartpad').style.display = 'none';
     querySelector('#dartpad-landing-page').style.display = 'none';
     return;
   }
