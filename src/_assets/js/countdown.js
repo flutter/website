f40a83476e5c81442a631411af9739402ffed242
Juan Gomez <juancamilog@google.com>
2019-10-22 09:06:49 -0500

    Add animations

--- src/_assets/js/countdown.js
+++ src/_assets/js/countdown.js
@@ -1,35 +1,38 @@
-(function () {
-    const timeNumber = document.querySelector('.time-number');
-    const eventTime = moment('23-10-2019 17:30:00', 'DD-MM-YYYY HH:mm:ss');
+const timeNumber = document.querySelector('.time-number');
+const eventTime = moment('21-12-2019 17:30:00', 'DD-MM-YYYY HH:mm:ss');
+
+function setTimer() {
+    const currentTime = moment();
+    const diffTime = eventTime - currentTime;
 
     function digitsHandler(number) {
         return number > 9 ? number : `0${number}`;
     }
 
-    setInterval(function() {
-        const currentTime = moment();
-        const diffTime = eventTime - currentTime;
+    if (diffTime > 0) {
+        // Duration left to the event
+        const duration = moment.duration(eventTime.diff(currentTime));
 
-        if (diffTime > 0) {
-            // Duration left to the event
-            const duration = moment.duration(eventTime.diff(currentTime));
+        //Get Days
+        const days = digitsHandler(duration.days());
 
-            //Get Days
-            const days = digitsHandler(duration.days());
+        //Get hours
+        const hours = digitsHandler(duration.hours());
 
-            //Get hours
-            const hours = digitsHandler(duration.hours());
+        //Get Minutes
+        const minutes = digitsHandler(duration.minutes());
 
-            //Get Minutes
-            const minutes = digitsHandler(duration.minutes());
+        //Get seconds
+        const seconds = digitsHandler(duration.seconds());
 
-            //Get seconds
-            const seconds = digitsHandler(duration.seconds());
+        timeNumber.innerText = `${days}:${hours}:${minutes}:${seconds}`;
+    } else {
+        timeNumber.innerText = "The event is right now";
+        clearInterval();
+    }
+}
 
-            timeNumber.innerText = `${days}:${hours}:${minutes}:${seconds}`;
-        } else {
-            time.innerText = "";
-            clearInterval();
-        }
-    }, 1000);
+(function () {
+    setTimer();
+    setInterval(setTimer, 1000);
 }());
