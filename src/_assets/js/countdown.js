558fac1f4a1056d7a6675ab335c642b398c9d776
Juan Gomez <juancamilog@google.com>
2019-10-22 11:56:18 -0500

    Change variables scope.

--- src/_assets/js/countdown.js
+++ src/_assets/js/countdown.js
@@ -1,38 +1,38 @@
-const timeNumber = document.querySelector('.time-number');
-const eventTime = moment('21-12-2019 17:30:00', 'DD-MM-YYYY HH:mm:ss');
-
-function setTimer() {
-    const currentTime = moment();
-    const diffTime = eventTime - currentTime;
-
-    function digitsHandler(number) {
-        return number > 9 ? number : `0${number}`;
-    }
-
-    if (diffTime > 0) {
-        // Duration left to the event
-        const duration = moment.duration(eventTime.diff(currentTime));
-
-        //Get Days
-        const days = digitsHandler(duration.days());
-
-        //Get hours
-        const hours = digitsHandler(duration.hours());
-
-        //Get Minutes
-        const minutes = digitsHandler(duration.minutes());
-
-        //Get seconds
-        const seconds = digitsHandler(duration.seconds());
-
-        timeNumber.innerText = `${days}:${hours}:${minutes}:${seconds}`;
-    } else {
-        timeNumber.innerText = "The event is right now";
-        clearInterval();
-    }
-}
-
 (function () {
+    const timeNumber = document.querySelector('.time-number');
+    const eventTime = moment('21-12-2019 17:30:00', 'DD-MM-YYYY HH:mm:ss');
+
+    function setTimer() {
+        const currentTime = moment();
+        const diffTime = eventTime - currentTime;
+
+        function digitsHandler(number) {
+            return number > 9 ? number : `0${number}`;
+        }
+
+        if (diffTime > 0) {
+            // Duration left to the event
+            const duration = moment.duration(eventTime.diff(currentTime));
+
+            //Get Days
+            const days = digitsHandler(duration.days());
+
+            //Get hours
+            const hours = digitsHandler(duration.hours());
+
+            //Get Minutes
+            const minutes = digitsHandler(duration.minutes());
+
+            //Get seconds
+            const seconds = digitsHandler(duration.seconds());
+
+            timeNumber.innerText = `${days}:${hours}:${minutes}:${seconds}`;
+        } else {
+            timeNumber.innerText = "The event is right now";
+            clearInterval();
+        }
+    }
+
     setTimer();
     setInterval(setTimer, 1000);
 }());
