8768f66ef273052364a57b8776a44372a4976a97
xster <xster@google.com>
2019-11-27 17:42:40 -0800

    Revise iOS add-to-app usage page (#3309)

--- src/_assets/js/main.js
+++ src/_assets/js/main.js
@@ -23,6 +23,11 @@ $(function () {
 
   // New (dash) tabs
   setupTabs($('#editor-setup'), 'io.flutter.tool-id');
+  setupTabs($('#engine-language'), 'io.flutter.tool-id');
+  setupTabs($('#vc-language'), 'io.flutter.tool-id');
+  setupTabs($('#no-engine-vc-language'), 'io.flutter.tool-id');
+  setupTabs($('#entrypoint-library-language'), 'io.flutter.tool-id');
+  setupTabs($('#initial-route-language'), 'io.flutter.tool-id');
   // Old tabs
   setupToolsTabs($('#tab-set-install'), 'tab-install-', 'io.flutter.tool-id');
   setupToolsTabs($('#tab-set-os'), 'tab-os-', null, getOS());
