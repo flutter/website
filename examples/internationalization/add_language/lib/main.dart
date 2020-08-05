75aa87e29f5479410b526d2d463e7bad89e75194
John Ryan <ryjohn@google.com>
2020-08-05 09:17:27 -0700

    Add missing overrides to add_language example (#4447)

--- examples/internationalization/add_language/lib/main.dart
+++ examples/internationalization/add_language/lib/main.dart
@@ -6,7 +6,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-class _BeMaterialLocalizationsDelegate extends LocalizationsDelegate<MaterialLocalizations> {
+class _BeMaterialLocalizationsDelegate
+    extends LocalizationsDelegate<MaterialLocalizations> {
   const _BeMaterialLocalizationsDelegate();
 
   @override
@@ -49,17 +50,17 @@ class BeMaterialLocalizations extends GlobalMaterialLocalizations {
     @required intl.NumberFormat decimalFormat,
     @required intl.NumberFormat twoDigitZeroPaddedFormat,
   }) : super(
-    localeName: localeName,
-    fullYearFormat: fullYearFormat,
-    compactDateFormat: compactDateFormat,
-    shortDateFormat: shortDateFormat,
-    mediumDateFormat: mediumDateFormat,
-    longDateFormat: longDateFormat,
-    yearMonthFormat: yearMonthFormat,
-    shortMonthDayFormat: shortMonthDayFormat,
-    decimalFormat: decimalFormat,
-    twoDigitZeroPaddedFormat: twoDigitZeroPaddedFormat,
-  );
+          localeName: localeName,
+          fullYearFormat: fullYearFormat,
+          compactDateFormat: compactDateFormat,
+          shortDateFormat: shortDateFormat,
+          mediumDateFormat: mediumDateFormat,
+          longDateFormat: longDateFormat,
+          yearMonthFormat: yearMonthFormat,
+          shortMonthDayFormat: shortMonthDayFormat,
+          decimalFormat: decimalFormat,
+          twoDigitZeroPaddedFormat: twoDigitZeroPaddedFormat,
+        );
 
   @override
   String get moreButtonTooltip => r'More';
@@ -134,7 +135,8 @@ class BeMaterialLocalizations extends GlobalMaterialLocalizations {
   String get pageRowsInfoTitleRaw => r'$firstRow–$lastRow of $rowCount';
 
   @override
-  String get pageRowsInfoTitleApproximateRaw => r'$firstRow–$lastRow of about $rowCount';
+  String get pageRowsInfoTitleApproximateRaw =>
+      r'$firstRow–$lastRow of about $rowCount';
 
   @override
   String get pasteButtonLabel => r'PASTE';
@@ -164,7 +166,8 @@ class BeMaterialLocalizations extends GlobalMaterialLocalizations {
   String get remainingTextFieldCharacterCountOne => r'1 character remaining';
 
   @override
-  String get remainingTextFieldCharacterCountOther => r'$remainingCount characters remaining';
+  String get remainingTextFieldCharacterCountOther =>
+      r'$remainingCount characters remaining';
 
   @override
   String get remainingTextFieldCharacterCountTwo => null;
@@ -245,12 +248,92 @@ class BeMaterialLocalizations extends GlobalMaterialLocalizations {
   String get viewLicensesButtonLabel => r'VIEW LICENSES';
 
   @override
-  List<String> get narrowWeekdays =>  const <String>['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  List<String> get narrowWeekdays =>
+      const <String>['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
   @override
   int get firstDayOfWeekIndex => 0;
 
-  static const LocalizationsDelegate<MaterialLocalizations> delegate = _BeMaterialLocalizationsDelegate();
+  static const LocalizationsDelegate<MaterialLocalizations> delegate =
+      _BeMaterialLocalizationsDelegate();
+
+  @override
+  String get calendarModeButtonLabel => r'Switch to calendar';
+
+  @override
+  String get dateHelpText => r'mm/dd/yyyy';
+
+  @override
+  String get dateInputLabel => r'Enter Date';
+
+  @override
+  String get dateOutOfRangeLabel => r'Out of range.';
+
+  @override
+  String get datePickerHelpText => r'SELECT DATE';
+
+  @override
+  String get dateRangeEndDateSemanticLabelRaw => r'End date $fullDate';
+
+  @override
+  String get dateRangeEndLabel => r'End Date';
+
+  @override
+  String get dateRangePickerHelpText => 'SELECT RANGE';
+
+  @override
+  String get dateRangeStartDateSemanticLabelRaw => 'Start date \$fullDate';
+
+  @override
+  String get dateRangeStartLabel => 'Start Date';
+
+  @override
+  String get dateSeparator => '/';
+
+  @override
+  String get dialModeButtonLabel => 'Switch to dial picker mode';
+
+  @override
+  String get inputDateModeButtonLabel => 'Switch to input';
+
+  @override
+  String get inputTimeModeButtonLabel => 'Switch to text input mode';
+
+  @override
+  String get invalidDateFormatLabel => 'Invalid format.';
+
+  @override
+  String get invalidDateRangeLabel => 'Invalid range.';
+
+  @override
+  String get invalidTimeLabel => 'Enter a valid time';
+
+  @override
+  String get licensesPackageDetailTextOther => '\$licenseCount licenses';
+
+  @override
+  String get saveButtonLabel => 'SAVE';
+
+  @override
+  String get selectYearSemanticsLabel => 'Select year';
+
+  @override
+  String get timePickerDialHelpText => 'SELECT TIME';
+
+  @override
+  String get timePickerHourLabel => 'Hour';
+
+  @override
+  String get timePickerInputHelpText => 'ENTER TIME';
+
+  @override
+  String get timePickerMinuteLabel => 'Minute';
+
+  @override
+  String get unspecifiedDate => 'Date';
+
+  @override
+  String get unspecifiedDateRange => 'Date Range';
 }
 
 class Home extends StatelessWidget {
@@ -266,7 +349,8 @@ class Home extends StatelessWidget {
             // values for tooltips and other strings related to accessibility.
             showDialog(
               context: context,
-              builder: (BuildContext context) => Dialog(child: Center(child: Text('Hello World'))),
+              builder: (BuildContext context) =>
+                  Dialog(child: Center(child: Text('Hello World'))),
             );
           },
           child: const Text('Show Dialog'),
@@ -284,7 +368,7 @@ void main() {
         GlobalMaterialLocalizations.delegate,
         BeMaterialLocalizations.delegate,
       ],
-      supportedLocales: [ const Locale('be', 'BY') ],
+      supportedLocales: [const Locale('be', 'BY')],
       home: Home(),
     ),
   );
