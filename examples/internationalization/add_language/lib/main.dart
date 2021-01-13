67e52c1efc7f705aa63fa14b6106abd201f08862
Shi-Hao Hong <shihaohong@google.com>
2021-01-13 17:38:07 +0800

    [l10n] Correct the "add a language" section (#5160)
    * Update sample app to reflect add a language steps
    
    * Fix add a language section

--- examples/internationalization/add_language/lib/main.dart
+++ examples/internationalization/add_language/lib/main.dart
@@ -1,364 +1,7 @@
-import 'dart:async';
-
-import 'package:intl/intl.dart' as intl;
-import 'package:intl/date_symbol_data_local.dart' as intl;
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-class _BeMaterialLocalizationsDelegate
-    extends LocalizationsDelegate<MaterialLocalizations> {
-  const _BeMaterialLocalizationsDelegate();
-
-  @override
-  bool isSupported(Locale locale) => locale.languageCode == 'be';
-
-  @override
-  Future<MaterialLocalizations> load(Locale locale) async {
-    final String localeName = intl.Intl.canonicalizedLocale(locale.toString());
-    await intl.initializeDateFormatting(localeName, null);
-    return SynchronousFuture<MaterialLocalizations>(
-      BeMaterialLocalizations(
-        localeName: localeName,
-        fullYearFormat: intl.DateFormat('y', localeName),
-        compactDateFormat: intl.DateFormat('yMd', localeName),
-        shortDateFormat: intl.DateFormat('yMMMd', localeName),
-        mediumDateFormat: intl.DateFormat('EEE, MMM d', localeName),
-        longDateFormat: intl.DateFormat('EEEE, MMMM d, y', localeName),
-        yearMonthFormat: intl.DateFormat('MMMM y', localeName),
-        shortMonthDayFormat: intl.DateFormat('MMM d'),
-        decimalFormat: intl.NumberFormat('#,##0.###', localeName),
-        twoDigitZeroPaddedFormat: intl.NumberFormat('00', localeName),
-      ),
-    );
-  }
-
-  @override
-  bool shouldReload(_BeMaterialLocalizationsDelegate old) => false;
-}
-
-class BeMaterialLocalizations extends GlobalMaterialLocalizations {
-  const BeMaterialLocalizations({
-    String localeName = 'be',
-    @required intl.DateFormat fullYearFormat,
-    @required intl.DateFormat compactDateFormat,
-    @required intl.DateFormat shortDateFormat,
-    @required intl.DateFormat mediumDateFormat,
-    @required intl.DateFormat longDateFormat,
-    @required intl.DateFormat yearMonthFormat,
-    @required intl.DateFormat shortMonthDayFormat,
-    @required intl.NumberFormat decimalFormat,
-    @required intl.NumberFormat twoDigitZeroPaddedFormat,
-  }) : super(
-          localeName: localeName,
-          fullYearFormat: fullYearFormat,
-          compactDateFormat: compactDateFormat,
-          shortDateFormat: shortDateFormat,
-          mediumDateFormat: mediumDateFormat,
-          longDateFormat: longDateFormat,
-          yearMonthFormat: yearMonthFormat,
-          shortMonthDayFormat: shortMonthDayFormat,
-          decimalFormat: decimalFormat,
-          twoDigitZeroPaddedFormat: twoDigitZeroPaddedFormat,
-        );
-
-  @override
-  String get moreButtonTooltip => r'More';
-
-  @override
-  String get aboutListTileTitleRaw => r'About $applicationName';
-
-  @override
-  String get alertDialogLabel => r'Alert';
-
-  @override
-  String get anteMeridiemAbbreviation => r'AM';
-
-  @override
-  String get backButtonTooltip => r'Back';
-
-  @override
-  String get cancelButtonLabel => r'CANCEL';
-
-  @override
-  String get closeButtonLabel => r'CLOSE';
-
-  @override
-  String get closeButtonTooltip => r'Close';
-
-  @override
-  String get collapsedIconTapHint => r'Expand';
-
-  @override
-  String get continueButtonLabel => r'CONTINUE';
-
-  @override
-  String get copyButtonLabel => r'COPY';
-
-  @override
-  String get cutButtonLabel => r'CUT';
-
-  @override
-  String get deleteButtonTooltip => r'Delete';
-
-  @override
-  String get dialogLabel => r'Dialog';
-
-  @override
-  String get drawerLabel => r'Navigation menu';
-
-  @override
-  String get expandedIconTapHint => r'Collapse';
-
-  @override
-  String get hideAccountsLabel => r'Hide accounts';
-
-  @override
-  String get licensesPageTitle => r'Licenses';
-
-  @override
-  String get modalBarrierDismissLabel => r'Dismiss';
-
-  @override
-  String get nextMonthTooltip => r'Next month';
-
-  @override
-  String get nextPageTooltip => r'Next page';
-
-  @override
-  String get okButtonLabel => r'OK';
-
-  @override
-  String get openAppDrawerTooltip => r'Open navigation menu';
-
-  @override
-  String get pageRowsInfoTitleRaw => r'$firstRow–$lastRow of $rowCount';
-
-  @override
-  String get pageRowsInfoTitleApproximateRaw =>
-      r'$firstRow–$lastRow of about $rowCount';
-
-  @override
-  String get pasteButtonLabel => r'PASTE';
-
-  @override
-  String get popupMenuLabel => r'Popup menu';
-
-  @override
-  String get postMeridiemAbbreviation => r'PM';
-
-  @override
-  String get previousMonthTooltip => r'Previous month';
-
-  @override
-  String get previousPageTooltip => r'Previous page';
-
-  @override
-  String get refreshIndicatorSemanticLabel => r'Refresh';
-
-  @override
-  String get remainingTextFieldCharacterCountFew => null;
-
-  @override
-  String get remainingTextFieldCharacterCountMany => null;
-
-  @override
-  String get remainingTextFieldCharacterCountOne => r'1 character remaining';
-
-  @override
-  String get remainingTextFieldCharacterCountOther =>
-      r'$remainingCount characters remaining';
-
-  @override
-  String get remainingTextFieldCharacterCountTwo => null;
-
-  @override
-  String get remainingTextFieldCharacterCountZero => r'No characters remaining';
-
-  @override
-  String get reorderItemDown => r'Move down';
-
-  @override
-  String get reorderItemLeft => r'Move left';
-
-  @override
-  String get reorderItemRight => r'Move right';
-
-  @override
-  String get reorderItemToEnd => r'Move to the end';
-
-  @override
-  String get reorderItemToStart => r'Move to the start';
-
-  @override
-  String get reorderItemUp => r'Move up';
-
-  @override
-  String get rowsPerPageTitle => r'Rows per page:';
-
-  @override
-  ScriptCategory get scriptCategory => ScriptCategory.englishLike;
-
-  @override
-  String get searchFieldLabel => r'Search';
-
-  @override
-  String get selectAllButtonLabel => r'SELECT ALL';
-
-  @override
-  String get selectedRowCountTitleFew => null;
-
-  @override
-  String get selectedRowCountTitleMany => null;
-
-  @override
-  String get selectedRowCountTitleOne => r'1 item selected';
-
-  @override
-  String get selectedRowCountTitleOther => r'$selectedRowCount items selected';
-
-  @override
-  String get selectedRowCountTitleTwo => null;
-
-  @override
-  String get selectedRowCountTitleZero => r'No items selected';
-
-  @override
-  String get showAccountsLabel => r'Show accounts';
-
-  @override
-  String get showMenuTooltip => r'Show menu';
-
-  @override
-  String get signedInLabel => r'Signed in';
-
-  @override
-  String get tabLabelRaw => r'Tab $tabIndex of $tabCount';
-
-  @override
-  TimeOfDayFormat get timeOfDayFormatRaw => TimeOfDayFormat.h_colon_mm_space_a;
-
-  @override
-  String get timePickerHourModeAnnouncement => r'Select hours';
-
-  @override
-  String get timePickerMinuteModeAnnouncement => r'Select minutes';
-
-  @override
-  String get viewLicensesButtonLabel => r'VIEW LICENSES';
-
-  @override
-  List<String> get narrowWeekdays =>
-      const <String>['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-
-  @override
-  int get firstDayOfWeekIndex => 0;
-
-  static const LocalizationsDelegate<MaterialLocalizations> delegate =
-      _BeMaterialLocalizationsDelegate();
-
-  @override
-  String get calendarModeButtonLabel => r'Switch to calendar';
-
-  @override
-  String get dateHelpText => r'mm/dd/yyyy';
-
-  @override
-  String get dateInputLabel => r'Enter Date';
-
-  @override
-  String get dateOutOfRangeLabel => r'Out of range.';
-
-  @override
-  String get datePickerHelpText => r'SELECT DATE';
-
-  @override
-  String get dateRangeEndDateSemanticLabelRaw => r'End date $fullDate';
-
-  @override
-  String get dateRangeEndLabel => r'End Date';
-
-  @override
-  String get dateRangePickerHelpText => 'SELECT RANGE';
-
-  @override
-  String get dateRangeStartDateSemanticLabelRaw => 'Start date \$fullDate';
-
-  @override
-  String get dateRangeStartLabel => 'Start Date';
-
-  @override
-  String get dateSeparator => '/';
-
-  @override
-  String get dialModeButtonLabel => 'Switch to dial picker mode';
-
-  @override
-  String get inputDateModeButtonLabel => 'Switch to input';
-
-  @override
-  String get inputTimeModeButtonLabel => 'Switch to text input mode';
-
-  @override
-  String get invalidDateFormatLabel => 'Invalid format.';
-
-  @override
-  String get invalidDateRangeLabel => 'Invalid range.';
-
-  @override
-  String get invalidTimeLabel => 'Enter a valid time';
-
-  @override
-  String get licensesPackageDetailTextOther => '\$licenseCount licenses';
-
-  @override
-  String get saveButtonLabel => 'SAVE';
-
-  @override
-  String get selectYearSemanticsLabel => 'Select year';
-
-  @override
-  String get timePickerDialHelpText => 'SELECT TIME';
-
-  @override
-  String get timePickerHourLabel => 'Hour';
-
-  @override
-  String get timePickerInputHelpText => 'ENTER TIME';
-
-  @override
-  String get timePickerMinuteLabel => 'Minute';
-
-  @override
-  String get unspecifiedDate => 'Date';
-
-  @override
-  String get unspecifiedDateRange => 'Date Range';
-}
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // This would fail if localizations were not provided for the
-            // app's locale. Although only a few Material widgets display
-            // localized strings, many expect to be able to find localized
-            // values for tooltips and other strings related to accessibility.
-            showDialog(
-              context: context,
-              builder: (BuildContext context) =>
-                  Dialog(child: Center(child: Text('Hello World'))),
-            );
-          },
-          child: const Text('Show Dialog'),
-        ),
-      ),
-    );
-  }
-}
+import 'nn_intl.dart';
 
 void main() {
   runApp(
@@ -366,10 +9,44 @@ void main() {
       localizationsDelegates: [
         GlobalWidgetsLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
-        BeMaterialLocalizations.delegate,
+        NnMaterialLocalizations.delegate, // Add the newly created delegate
       ],
-      supportedLocales: [const Locale('be', 'BY')],
+      supportedLocales: [
+        const Locale('en', 'US'),
+        const Locale('nn'),
+      ],
       home: Home(),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Localizations.override(
+      context: context,
+      locale: const Locale('nn'),
+      child: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(
+          child: ListTile(
+            leading: const Icon(Icons.change_history),
+            title: const Text('Change history'),
+            onTap: () {
+              Navigator.pop(context); // close the drawer
+            },
+          ),
+        ),
+        body: const Center(
+          child: Padding(
+            padding: EdgeInsets.all(50.0),
+            child: Text(
+              'Long press hamburger icon in the app bar (aka the drawer menu)'
+              'to see a localized tooltip for the `nn` locale. '
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
--- src/docs/development/accessibility-and-localization/internationalization.md
+++ src/docs/development/accessibility-and-localization/internationalization.md
@@ -529,10 +529,11 @@ that analyzes the source code for classes that contain
 An app that needs to support a language that's not included in
 [`GlobalMaterialLocalizations`][] has to do some extra work:
 it must provide about 70 translations ("localizations")
-for words or phrases.
+for words or phrases and the date patterns and symbols for the
+locale.
 
 See the following for an example of how to add
-support for the Belarusian language.
+support for the Norwegian Nynorsk language.
 
 A new `GlobalMaterialLocalizations` subclass defines the
 localizations that the Material library depends on.
@@ -541,19 +542,19 @@ as factory for the `GlobalMaterialLocalizations` subclass,
 must also be defined.
 
 Here's the source code for the complete [`add_language`][] example,
-minus the actual Belarusian translations.
+minus the actual Nynorsk translations.
 
 The locale-specific `GlobalMaterialLocalizations` subclass
-is called `BeMaterialLocalizations`,
+is called `NnMaterialLocalizations`,
 and the `LocalizationsDelegate` subclass is
-`_BeMaterialLocalizationsDelegate`.
-The value of `BeMaterialLocalizations.delegate`
+`_NnMaterialLocalizationsDelegate`.
+The value of `NnMaterialLocalizations.delegate`
 is an instance of the delegate, and is all
 that's needed by an app that uses these localizations.
 
 The delegate class includes basic date and number format
 localizations. All of the other localizations are defined by `String`
-valued property getters in `BeMaterialLocalizations`, like this:
+valued property getters in `NnMaterialLocalizations`, like this:
 
 <!-- skip -->
 ```dart
@@ -571,7 +572,7 @@ String get closeButtonLabel => r'CLOSE';
 
 These are the English translations, of course.
 To complete the job you need to change the return
-value of each getter to an appropriate Belarusian string.
+value of each getter to an appropriate Nynorsk string.
 
 The getters return "raw" Dart strings that have an r prefix,
 like `r'About $applicationName'`,
@@ -590,14 +591,66 @@ String aboutListTileTitle(String applicationName) {
 }
 ```
 
+The date patterns and symbols of the locale will also need to
+be specified. In the source code, the date patterns and symbols
+are defined like this:
+
+<!-- skip -->
+```dart
+const nnLocaleDatePatterns = {
+  'd': 'd.',
+  'E': 'ccc',
+  //...
+}
+
+const nnDateSymbols = {
+  'NAME': 'nn',
+  'ERAS': <dynamic>[
+    'f.Kr.',
+    'e.Kr.',
+  ],
+  // ...
+}
+```
+
+These will need to be modified for the locale to use the correct
+date formatting. Unfortunately, since the `intl` library does
+not share the same flexibility for number formatting, the formatting
+for an existing locale will have to be used as a substitute in
+`_NnMaterialLocalizationsDelegate`:
+
+<!-- skip -->
+```dart
+class _NnMaterialLocalizationsDelegate extends LocalizationsDelegate<MaterialLocalizations> {
+  // ...
+  @override
+  Future<MaterialLocalizations> load(Locale locale) async {
+    return SynchronousFuture<MaterialLocalizations>(
+      NnMaterialLocalizations(
+        localeName: localeName,
+        // The `intl` library's NumberFormat class is generated from CLDR data
+        // (see https://github.com/dart-lang/intl/blob/master/lib/number_symbols_data.dart).
+        // Unfortunately, there is no way to use a locale that isn't defined in
+        // this map and the only way to work around this is to use a listed
+        // locale's NumberFormat symbols. So, here we use the number formats
+        // for 'en_US' instead.
+        decimalFormat: intl.NumberFormat('#,##0.###', 'en_US'),
+        twoDigitZeroPaddedFormat: intl.NumberFormat('00', 'en_US'),
+        //...
+      ),
+    );
+  }
+}
+```
+
 For more information about localization strings, see the
 [flutter_localizations README][].
 
 Once you've implemented your language-specific subclasses of
 `GlobalMaterialLocalizations` and `LocalizationsDelegate`,
 you just need to add the language and a delegate instance to your app.
-Here's some code that sets the app's language to Belarusian and
-adds the `BeMaterialLocalizations` delegate instance to the app's
+Here's some code that sets the app's language to Nynorsk and
+adds the `NnMaterialLocalizations` delegate instance to the app's
 `localizationsDelegates` list:
 
 <!-- skip -->
@@ -606,10 +659,10 @@ MaterialApp(
   localizationsDelegates: [
     GlobalWidgetsLocalizations.delegate,
     GlobalMaterialLocalizations.delegate,
-    BeMaterialLocalizations.delegate,
+    NnMaterialLocalizations.delegate,
   ],
   supportedLocales: [
-    const Locale('be', 'BY')
+    const Locale('nn')
   ],
   home: ...
 )
