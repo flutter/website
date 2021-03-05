f366454e2ed8c2935f57d3ffa5357296d5423702
Miguel Beltran <m@beltran.work>
2021-03-05 16:14:03 +0100

    migrated to null safety

--- null_safety_examples/internationalization/gen_l10n_example/lib/main.dart
+++ null_safety_examples/internationalization/gen_l10n_example/lib/main.dart
@@ -42,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // according to the system locale of the target platform.
         // Switching between English and Spanish locales should
         // cause this text to update.
-        title: Text(AppLocalizations.of(context).helloWorld),
+        title: Text(AppLocalizations.of(context)!.helloWorld),
       ),
     );
   }
