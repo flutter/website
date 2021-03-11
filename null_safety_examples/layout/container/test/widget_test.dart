166e81cfec617b08bd75203f5600cedacae3fde7
Miguel Beltran <m@beltran.work>
2021-03-11 04:20:56 +0100

    Migration of "layout" examples to null safety (#5413)
    * copy of card_and_stack
    
    * update to null safety
    
    * copy of container
    
    * migrated to null safety
    
    * copy of grid_and_list
    
    * migrated to null safety
    
    * copy of lakes
    
    * migrated to null safety
    
    * copy of non-material
    
    * migrated to null safety
    
    * copy of pavlova
    
    * migrated to null safety
    
    * copy of row column
    
    * migrated to null safety
    
    * copy of sizing
    
    * migrated to null safety

---
+++ null_safety_examples/layout/container/test/widget_test.dart
@@ -0,0 +1,13 @@
+// Basic Flutter widget test. Learn more at https://flutter.io/docs/testing.
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:layout/main.dart';
+
+void main() {
+  testWidgets('Example app smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp());
+
+    expect(find.text('Flutter layout demo'), findsOneWidget);
+  });
+}
