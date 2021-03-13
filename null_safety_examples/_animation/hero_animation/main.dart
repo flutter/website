f7333d927e576f2753ac1bf23ac753c060fdbc2b
Miguel Beltran <m@beltran.work>
2021-03-13 01:34:02 +0100

    Migrated "examples/_animation" to null safety (#5465)
    * copy of examples/_animation
    
    * migrated _animation folder

---
+++ null_safety_examples/_animation/hero_animation/main.dart
@@ -0,0 +1,90 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Tap on the source route’s photo to push a new route,
+// containing the same photo at a different location and scale.
+// Return to the previous route by tapping the image, or by using the
+// device’s back-to-the-previous-screen gesture.
+// You can slow the transition using the timeDilation property.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
+
+class PhotoHero extends StatelessWidget {
+  const PhotoHero({
+    Key? key,
+    required this.photo,
+    this.onTap,
+    required this.width,
+  }) : super(key: key);
+
+  final String photo;
+  final VoidCallback? onTap;
+  final double width;
+
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+      child: Hero(
+        tag: photo,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            child: Image.asset(
+              photo,
+              fit: BoxFit.contain,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HeroAnimation extends StatelessWidget {
+  Widget build(BuildContext context) {
+    timeDilation = 10.0; // 1.0 means normal animation speed.
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Basic Hero Animation'),
+      ),
+      body: Center(
+        child: PhotoHero(
+          photo: 'images/flippers-alpha.png',
+          width: 300.0,
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute<void>(
+              builder: (BuildContext context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    title: const Text('Flippers Page'),
+                  ),
+                  body: Container(
+                    // Set background to blue to emphasize that it's a new route.
+                    color: Colors.lightBlueAccent,
+                    padding: const EdgeInsets.all(16.0),
+                    alignment: Alignment.topLeft,
+                    child: PhotoHero(
+                      photo: 'images/flippers-alpha.png',
+                      width: 100.0,
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                );
+              }
+            ));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(home: HeroAnimation()));
+}
