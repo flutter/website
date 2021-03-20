d1fba813317586616afc9e3e1863fc3199498ed4
Shams Zakhour (ignore Sfshaza) <44418985+sfshaza2@users.noreply.github.com>
2021-03-20 13:40:54 -0700

    Update null_safety_examples/cookbook/forms/validation/lib/main.dart
    Co-authored-by: John Ryan <ryjohn@google.com>

--- null_safety_examples/cookbook/forms/validation/lib/main.dart
+++ null_safety_examples/cookbook/forms/validation/lib/main.dart
@@ -61,10 +61,13 @@ class MyCustomFormState extends State<MyCustomForm> {
             // #docregion ElevatedButton
             child: ElevatedButton(
               onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_formKey.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
+                var state = _formKey.currentState;
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (state != null && state.validate()) {
+                  // If the form is valid, display a snackbar. In the real
+                  // world, you'd often call a server or save the information in
+                  // a database.
                   ScaffoldMessenger.of(context)
                       .showSnackBar(SnackBar(content: Text('Processing Data')));
                 }
