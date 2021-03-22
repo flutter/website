ad9fc8bffddab33fded978d4d0e54bfd7f3262fe
Miguel Beltran <m@beltran.work>
2021-03-22 08:28:38 +0100

    Revert "Update null_safety_examples/cookbook/forms/validation/lib/main.dart"
    This reverts commit d1fba813317586616afc9e3e1863fc3199498ed4.

--- null_safety_examples/cookbook/forms/validation/lib/main.dart
+++ null_safety_examples/cookbook/forms/validation/lib/main.dart
@@ -61,13 +61,10 @@ class MyCustomFormState extends State<MyCustomForm> {
             // #docregion ElevatedButton
             child: ElevatedButton(
               onPressed: () {
-                var state = _formKey.currentState;
-                // Validate returns true if the form is valid, or false
-                // otherwise.
-                if (state != null && state.validate()) {
-                  // If the form is valid, display a snackbar. In the real
-                  // world, you'd often call a server or save the information in
-                  // a database.
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
                   ScaffoldMessenger.of(context)
                       .showSnackBar(SnackBar(content: Text('Processing Data')));
                 }
