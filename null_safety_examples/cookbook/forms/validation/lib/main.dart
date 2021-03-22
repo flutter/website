195b2270a3f7f4c85eb11aaeb033e6e53042714e
Miguel Beltran <m@beltran.work>
2021-03-22 20:12:54 +0100

    Migrated forms validation cookbook to null safety (#5515)
    * Migrated forms validation cookbook
    
    * remove unused deps
    
    * simplified code sample project
    
    * Update null_safety_examples/cookbook/forms/validation/lib/main.dart
    
    Co-authored-by: John Ryan <ryjohn@google.com>
    
    * Revert "Update null_safety_examples/cookbook/forms/validation/lib/main.dart"
    
    This reverts commit d1fba813317586616afc9e3e1863fc3199498ed4.
    
    Co-authored-by: Shams Zakhour (ignore Sfshaza) <44418985+sfshaza2@users.noreply.github.com>
    Co-authored-by: John Ryan <ryjohn@google.com>

---
+++ null_safety_examples/cookbook/forms/validation/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Form Validation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+// Create a Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+// Create a corresponding State class.
+// This class holds data related to the form.
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          // #docregion TextFormField
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          // #enddocregion TextFormField
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            // #docregion ElevatedButton
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: Text('Submit'),
+            ),
+            // #enddocregion ElevatedButton
+          ),
+        ],
+      ),
+    );
+  }
+}
