8b83ceb3a43ce4ec8cc632c2a2e08820859ad818
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2021-04-05 15:22:08 +0200

    Migrated hero-animations to null safety

---
+++ null_safety_examples/cookbook/navigation/hero_animations/lib/main_original.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (_) {
+            return DetailScreen();
+          }));
+        },
+        child: Image.network(
+          'https://picsum.photos/250?image=9',
+        ),
+      ),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Center(
+          child: Image.network(
+            'https://picsum.photos/250?image=9',
+          ),
+        ),
+      ),
+    );
+  }
+}
