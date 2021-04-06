a7c170047486b4612a6b75b1f14a8ddaf04d83d4
Miguel Beltran <m@beltran.work>
2021-04-06 19:34:41 +0200

    Migrated named-routes.md to null safety (#5588)
    * Migrated named-routes.md to null safety
    
    * added comments
    
    Co-authored-by: Miquel Beltran <miquelbeltran@users.noreply.github.com>

---
+++ null_safety_examples/cookbook/navigation/named_routes/lib/main_original.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Launch screen'),
+          onPressed: () {
+            // Navigate to the second screen when tapped.
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first screen when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
