9da32180de67e0d74dbd0ea30083e735860a4b33
Miquel Beltran <miquelbeltran@users.noreply.github.com>
2021-04-23 15:08:15 +0200

    migrated physics simulation to null safety

---
+++ null_safety_examples/cookbook/animation/physics_simulation/lib/step3.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MaterialApp(home: PhysicsCardDragDemo()));
+}
+
+class PhysicsCardDragDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: DraggableCard(
+        child: FlutterLogo(
+          size: 128,
+        ),
+      ),
+    );
+  }
+}
+
+class DraggableCard extends StatefulWidget {
+  final Widget child;
+  DraggableCard({required this.child});
+
+  @override
+  _DraggableCardState createState() => _DraggableCardState();
+}
+
+// #docregion animation
+class _DraggableCardState extends State<DraggableCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Alignment> _animation;
+  Alignment _dragAlignment = Alignment.center;
+  // #enddocregion animation
+
+  // #docregion initState
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+    _controller.addListener(() {
+      setState(() {
+        _dragAlignment = _animation.value;
+      });
+    });
+  }
+  // #enddocregion initState
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    // #docregion gesture
+    return GestureDetector(
+      onPanDown: (details) {
+        _controller.stop();
+      },
+      onPanUpdate: (details) {
+        setState(() {
+          _dragAlignment += Alignment(
+            details.delta.dx / (size.width / 2),
+            details.delta.dy / (size.height / 2),
+          );
+        });
+      },
+      onPanEnd: (details) {
+        _runAnimation();
+      },
+      // #docregion align
+      child: Align(
+        alignment: _dragAlignment,
+        child: Card(
+          child: widget.child,
+        ),
+      ),
+      // #enddocregion align
+    );
+    // #enddocregion gesture
+  }
+
+  // #docregion runAnimation
+  void _runAnimation() {
+    _animation = _controller.drive(
+      AlignmentTween(
+        begin: _dragAlignment,
+        end: Alignment.center,
+      ),
+    );
+    _controller.reset();
+    _controller.forward();
+  }
+  // #enddocregion runAnimation
+}
