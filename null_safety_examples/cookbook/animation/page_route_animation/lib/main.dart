938e89e4cd622c65c675d69f0a3c2b28bcaef935
Miguel Beltran <m@beltran.work>
2021-04-27 00:29:27 +0200

    migrate page-route-animation to null safety (#5677)
    Co-authored-by: Miquel Beltran <miquelbeltran@users.noreply.github.com>

---
+++ null_safety_examples/cookbook/animation/page_route_animation/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(
+    MaterialApp(
+      home: Page1(),
+    ),
+  );
+}
+
+class Page1 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Go!'),
+          onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
+    // #docregion transitionsBuilder
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      // #docregion Tween
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      // #enddocregion Tween
+
+      // #docregion SlideTransition
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
+      // #enddocregion SlideTransition
+    },
+    // #enddocregion transitionsBuilder
+  );
+}
+
+class Page2 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Page 2'),
+      ),
+    );
+  }
+}
