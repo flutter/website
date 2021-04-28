e333161455901464c4e7193b52da0f1162351366
Miguel Beltran <m@beltran.work>
2021-04-28 04:59:05 +0200

    Migrated retreive-input.md to null safety (#5698)

---
+++ null_safety_examples/cookbook/forms/retreive_input/lib/starter.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// #docregion Starter
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Fill this out in the next step.
+    return Container();
+  }
+}
+// #enddocregion Starter
