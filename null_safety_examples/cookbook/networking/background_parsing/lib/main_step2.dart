f243affc209b0d2c2394f41be07af8ad4148534f
Miguel Beltran <m@beltran.work>
2021-05-06 15:43:50 +0200

    fix typo

--- null_safety_examples/cookbook/networking/background_parsing/lib/main_step2.dart
+++ null_safety_examples/cookbook/networking/background_parsing/lib/main_step2.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 
 import 'package:http/http.dart' as http;
 
-// #docregion fetchPhoto
+// #docregion fetchPhotos
 Future<http.Response> fetchPhotos(http.Client client) async {
   return client.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
 }
-// #enddocregion fetchPhoto
+// #enddocregion fetchPhotos
