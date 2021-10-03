0c1ca98393e6abc8cb1ab63827926a7f5986b07d
Tirth <pateltirth454@gmail.com>
2021-10-03 17:46:09 +0530

    declare variable

--- examples/cookbook/plugins/picture_using_camera/lib/main_step3.dart
+++ examples/cookbook/plugins/picture_using_camera/lib/main_step3.dart
@@ -41,6 +41,7 @@ class TakePictureScreen extends StatefulWidget {
 
 class TakePictureScreenState extends State<TakePictureScreen> {
   late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
   @override
   void initState() {
