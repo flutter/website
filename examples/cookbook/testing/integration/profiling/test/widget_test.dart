6c704d3842f4658e211cdc54b160cf3a446b3cec
Miguel Beltran <m@beltran.work>
2021-11-26 15:15:23 +0100

    copied testing/widget/scrolling into testing/integration/profiling

---
+++ examples/cookbook/testing/integration/profiling/test/widget_test.dart
@@ -0,0 +1,36 @@
+// #docregion ScrollWidgetTest
+
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:scrolling/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MyApp(
+      items: List<String>.generate(10000, (i) => "Item $i"),
+    ));
+
+    final listFinder = find.byType(Scrollable);
+    final itemFinder = find.byKey(const ValueKey('item_50_text'));
+
+    // Scroll until the item to be found appears.
+    await tester.scrollUntilVisible(
+      itemFinder,
+      500.0,
+      scrollable: listFinder,
+    );
+
+    // Verify that the item contains the correct text.
+    expect(itemFinder, findsOneWidget);
+  });
+}
+// #enddocregion ScrollWidgetTest
