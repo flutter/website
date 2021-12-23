afe3c2b9539273494370630558e654557b20c6a2
Khanh Nguyen <khanhnwin@gmail.com>
2021-12-23 11:22:41 -0500

    Code Excerpts Migration for /cookbook/forms (#6604)
    * Migrate CallbackShortcuts code snippet to new snippet system
    
    * Resolve code snippet reference errors from PR #6296
    
    * Change cookbook links to scrolling.md to fix 404s
    
    * Update prev/next paths to match https://github.com/flutter/website/blob/main/src/docs/cookbook/index.md
    
    * fixed path to /docs/cookbook/testing/integration/introduction in profiling.md
    
    * add firebase redirect for '/go/android-plugin-migration.' to prevent 404
    
    * migrated 3 code excerpts for ./docs/development/ui/layout/building-adaptive-apps.md using example code from https://github.com/gskinnerTeam/flutter-adaptive-demo
    
    * migrated remaining available code snippets from adaptive_app_demos
    
    * add code excerpt references for building-adaptive-apps.md
    
    * fixed code excerpt start + end
    
    * building adaptive apps code excerpts migration
    
    * code excerpt migration for ---
    ---
    assets-and-images.md
    
    * building adaptive apps code excerpts
    
    * update mention of github repo for the adaptive app repo and mention that snippets may not make total sense in the repo
    
    * clean up visual density snippet
    
    * update listener excerpt with ListView
    
    * adding trailing comma to make code nicer
    
    * Remove unnecessary/unused code from adaptive_app_demos
    
    * Code excerpts building adaptive apps - platform channels
    
    * enable prefer_single_quotes
    
    * replace double quotes with single quotes in platform_integration project dart files
    
    * enable prefer_single_quotes for plugin_api_migration project
    
    * Update analysis rules for new projects to enable prefer_single_quotes
    
    * refresh code excerpts with single quotes
    
    * Migrate snippets for development/platform-integration/platform-views.md to excerpts
    
    * Migrate cookbook/design/snackbars snippets to excerpts
    
    * Add flutter project for cookbook/design/fonts with code excerpts
    
    * code excerpts for cookbook/design/tabs
    
    * Code excerpt partials for /cookbook/design/orientation
    
    * added flutter project with code excerpt for /cookbook/design/package-fonts
    
    * Update examples/cookbook/design/fonts/analysis_options.yaml
    
    Co-authored-by: Shams Zakhour (ignore Sfshaza) <44418985+sfshaza2@users.noreply.github.com>
    
    * Update examples/cookbook/design/fonts/analysis_options.yaml
    
    Co-authored-by: Shams Zakhour (ignore Sfshaza) <44418985+sfshaza2@users.noreply.github.com>
    
    * Update examples/cookbook/design/fonts/analysis_options.yaml
    
    Co-authored-by: Shams Zakhour (ignore Sfshaza) <44418985+sfshaza2@users.noreply.github.com>
    
    * Update examples/cookbook/design/fonts/analysis_options.yaml
    
    Co-authored-by: Shams Zakhour (ignore Sfshaza) <44418985+sfshaza2@users.noreply.github.com>
    
    * Integrate Shams PR feedback for analysis_options.yaml
    
    * remove autogenerated comments form pubspec.yaml
    
    * Remove autogenerated comments from pubspec.yaml and analysis_options.yaml
    
    * add new line at the end of files
    
    * code excerpts for cookbook/forms/text-input
    
    * remove child: from text-input excerpts
    
    * Code excerpts for cookbook/forms/focus
    
    * code excerpts for cookbook/forms/retrieve-input
    
    * refresh codelab excerpts
    
    * add newline at end of pubspec
    
    * fix typo
    
    * refresh code snippets
    
    Co-authored-by: Shams Zakhour (ignore Sfshaza) <44418985+sfshaza2@users.noreply.github.com>

---
+++ examples/cookbook/forms/retrieve_input/lib/step2.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// Define a custom Form widget.
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  _MyCustomFormState createState() => _MyCustomFormState();
+}
+
+// Define a corresponding State class.
+// This class holds the data related to the Form.
+class _MyCustomFormState extends State<MyCustomForm> {
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Fill this out in the next step.
+    // #docregion TextFieldController
+    return TextField(
+      controller: myController,
+    );
+    // #enddocregion TextFieldController
+  }
+}
