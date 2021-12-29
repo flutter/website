0057266cc1e9a4aed4cb1ea2b67aaafa663d5d5e
Khanh Nguyen <khanhnwin@gmail.com>
2021-12-29 11:24:43 -0500

    add constructor

--- examples/cookbook/lists/grid_lists/lib/main.dart
+++ examples/cookbook/lists/grid_lists/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     const title = 'Grid List';
