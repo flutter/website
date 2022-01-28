0b70a63d64a1781597508a609062eeb2756c9958
Parker Lougheed <parlough@gmail.com>
2022-01-28 13:31:58 -0600

    Fix design doc list not updating (#6739)
    * Fix design doc list not updating
    
    Fixes #6737
    
    * Account for new type of redirects which don't have source

--- src/assets/js/go_link_index.js
+++ src/assets/js/go_link_index.js
@@ -1,8 +1,8 @@
 // Fetches Go link redirects from firebase.json
-// Since we can't access it from the site directly, fetch it from GitHub master.
+// Since we can't access it from the site directly, fetch it from GitHub main.
 const fetchGoLinks = function (callback, errorCallback) {
   // OS: windows, macos, linux
-  var url = "https://raw.githubusercontent.com/flutter/website/master/firebase.json";
+  const url = "https://raw.githubusercontent.com/flutter/website/main/firebase.json";
   $.ajax({
     type: "GET",
     url: url,
@@ -21,9 +21,12 @@ function updateList(links) {
   }
 
   links.hosting.redirects.filter(
-    redirect => redirect.source.startsWith('/go/') && redirect.source != '/go/template'
+      redirect =>
+          redirect.source &&
+          redirect.source.startsWith('/go/') &&
+          redirect.source !== '/go/template'
   ).forEach(
-    redirect => go_links.append(`<li><a href="${redirect.destination}">${redirect.source}</a></li>`)
+      redirect => go_links.append(`<li><a href="${redirect.destination}">${redirect.source}</a></li>`)
   );
 }
 
