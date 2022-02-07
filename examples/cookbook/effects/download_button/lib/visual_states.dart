bdafd1b233048893a94045a578fe65fa711d20ed
Khanh Nguyen <khanhnwin@gmail.com>
2022-02-07 16:57:41 -0600

    new Flutter project for cookbook/effects/download-button

---
+++ examples/cookbook/effects/download_button/lib/visual_states.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+// #docregion VisualStates
+enum DownloadStatus {
+  notDownloaded,
+  fetchingDownload,
+  downloading,
+  downloaded,
+}
+
+@immutable
+class DownloadButton extends StatelessWidget {
+  const DownloadButton({
+    Key? key,
+    required this.status,
+    this.transitionDuration = const Duration(
+      milliseconds: 500,
+    ),
+  }) : super(key: key);
+
+  final DownloadStatus status;
+  final Duration transitionDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO:
+    return const SizedBox();
+  }
+}
+// #enddocregion VisualStates
