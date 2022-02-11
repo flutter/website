03931f91f607f1ddd375c9e675bd2833513f0dc8
Khanh Nguyen <khanhnwin@gmail.com>
2022-02-11 14:46:25 -0600

    lib/main.dart

---
+++ examples/cookbook/effects/shimmer_loading/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class CircleListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+      child: Container(
+        width: 54,
+        height: 54,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          shape: BoxShape.circle,
+        ),
+        child: ClipOval(
+          child: Image.network(
+            'https://flutter'
+            '.dev/docs/cookbook/img-files/effects/split-check/Avatar1.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
