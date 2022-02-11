4184c38c739c9d8f2a81513ef5aa6d8d8d6da725
Khanh Nguyen <khanhnwin@gmail.com>
2022-02-11 12:20:31 -0600

    Migrate code excerpts for cookbook/effects/gradient-bubbles (#6806)
    * Migrate CallbackShortcuts code snippet to new snippet system
    
    * Resolve code snippet reference errors from PR #6296
    
    * Change cookbook links to scrolling.md to fix 404s
    
    * Update prev/next paths to match https://github.com/flutter/website/blob/main/src/docs/cookbook/index.md
    
    * fixed path to /docs/cookbook/testing/integration/introduction in profiling.md
    
    * add firebase redirect for '/go/android-plugin-migration.' to prevent 404
    
    * migrated 3 code excerpts for ./docs/development/ui/layout/building-adaptive-apps.md using example code from https://github.com/gskinnerTeam/flutter-adaptive-demo
    
    * migrated remaining available code snippets from adaptive_app_demos
    
    * add code excerpt references for building-adaptive-apps.md
    
    * fixed code excerpt start + end
    
    * building adaptive apps code excerpts migration
    
    * code excerpt migration for ---
    ---
    assets-and-images.md
    
    * Update PULL_REQUEST_TEMPLATE.md
    
    Updating template with freeze warning for Friday, November 5.
    
    * Update PULL_REQUEST_TEMPLATE.md
    
    Remove line break in first line.
    
    * building adaptive apps code excerpts
    
    * update mention of github repo for the adaptive app repo and mention that snippets may not make total sense in the repo
    
    * clean up visual density snippet
    
    * update listener excerpt with ListView
    
    * adding trailing comma to make code nicer
    
    * flutter.dev freeze PR Template
    
    * fix linkcheck add re2 and update javascript regex
    
    * update dockerfile install re2
    
    * Remove unnecessary/unused code from adaptive_app_demos
    
    * Code excerpts building adaptive apps - platform channels
    
    * excerpts for cookbook/networking/send-data
    
    * one excerpt for cookbook/networking/delete-data
    
    * excerpt for cookbook/networking/fetch-data
    
    * excerpt for cookbook/networking/update-data
    
    * last excerpt for cookbook/navigation/passing-data
    
    * navigation_basics
    
    * code excerpts for cookbook/navigation/navigation_basics
    
    * clean up pubspec
    
    * clean up analysis options
    
    * excerpts for cookbook/animation/opacity-animation
    
    * new examples project for cookbook/testing/unit/introduction
    
    * excerpts for cookbook/testing/unit/introduction
    
    * code excerpt steps for cookbook/testing/widget/tap-drag
    
    * excerpts for cookbook/testing/integration/introduction
    
    * excerpts for cookbook/gestures/dismissible
    
    * add newline at end of pubspec
    
    * true newline at the end of the pubspec
    
    * code excerpts for cookbook/persistence/key-value
    
    * import flutter_test
    
    * code excerpt for cookbook/persistence/sqlite
    
    * code excerpts for cookbook/maintenance/error-reporting
    
    * refresh code excerpts
    
    * Remove unnecessary sidenav links for Deployment + Data & Backend
    
    * resolve merge conflict
    
    * fix c_interop.dart quotes and refresh code excerpt
    
    * Revert "code excerpts for cookbook/maintenance/error-reporting"
    
    This reverts commit 21464e5a5f80af2c59e3b8d513f7cda0e5c5b55a.
    
    * code excerpts for cookbook/maintenance/error-reporting
    
    * update codelabs submodule
    
    * resolve weird diff markers
    
    * add newline at end of file
    
    * updated examples/codelabs to latest commit 2f4462a
    
    * remove extra newline
    
    * update readme with a test
    
    * remove test line
    
    * excerpts for cookbook/effects/gradient_bubbles
    
    * clean up unused import and added a dart analyze ignore
    
    * pubspec and analysis_options
    
    * Update examples/cookbook/effects/gradient_bubbles/lib/main.dart
    
    Co-authored-by: Parker Lougheed <parlough@gmail.com>
    
    * refresh code excerpts
    
    Co-authored-by: Parker Lougheed <parlough@gmail.com>

---
+++ examples/cookbook/effects/gradient_bubbles/lib/bubble_painter_empty.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: unused_field
+
+import 'package:flutter/material.dart';
+
+// #docregion BubblePainterEmpty
+@immutable
+class BubbleBackground extends StatelessWidget {
+  const BubbleBackground({
+    Key? key,
+    required this.colors,
+    this.child,
+  }) : super(key: key);
+
+  final List<Color> colors;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: BubblePainter(
+        colors: colors,
+      ),
+      child: child,
+    );
+  }
+}
+
+class BubblePainter extends CustomPainter {
+  BubblePainter({
+    required List<Color> colors,
+  }) : _colors = colors;
+
+  final List<Color> _colors;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO:
+  }
+
+  @override
+  bool shouldRepaint(BubblePainter oldDelegate) {
+    // TODO:
+    return false;
+  }
+}
+// #enddocregion BubblePainterEmpty
