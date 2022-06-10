da8bb3b2cf4cc0307b270e369683b1ecb5ff8427
Khanh Nguyen <khanhnwin@gmail.com>
2022-06-10 16:44:36 -0400

    excerpts for testing/common_errors

---
+++ examples/testing/common_errors/lib/renderflex_overflow.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class ProblemWidget extends StatelessWidget {
+  const ProblemWidget({Key? key}) : super(key: key);
+
+  @override
+  // #docregion Problem
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Icon(Icons.message),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Title", style: Theme.of(context).textTheme.headline4),
+              Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed"
+                  " do eiusmod tempor incididunt ut labore et dolore magna "
+                  "aliqua. Ut enim ad minim veniam, quis nostrud "
+                  "exercitation ullamco laboris nisi ut aliquip ex ea "
+                  "commodo consequat."),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+  // #enddocregion Problem
+}
+
+class SolutionWidget extends StatelessWidget {
+  const SolutionWidget({Key? key}) : super(key: key);
+
+  @override
+  // #docregion Build
+  Widget build(BuildContext context) {
+    return Container(
+      // #docregion Fix
+      child: Row(
+        children: [
+          Icon(Icons.message),
+          Expanded(
+            child: Column(
+                // code omitted
+                ),
+          ),
+        ],
+      ),
+      // #enddocregion Fix
+    );
+  }
+  // #enddocregion Build
+}
