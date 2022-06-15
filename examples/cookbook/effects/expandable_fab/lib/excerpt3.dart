74c520a90586e8636e8962627a0ed260c9d3c134
Ross Llewallyn <rossllewallyn@protonmail.com>
2022-06-15 11:45:30 -0400

    removed underscore from ExpandableFabState

--- examples/cookbook/effects/expandable_fab/lib/excerpt3.dart
+++ examples/cookbook/effects/expandable_fab/lib/excerpt3.dart
@@ -73,11 +73,11 @@ class ExpandableFab extends StatefulWidget {
   final List<Widget> children;
 
   @override
-  _ExpandableFabState createState() => _ExpandableFabState();
+  ExpandableFabState createState() => ExpandableFabState();
 }
 
 // #docregion ExpandableFabState3
-class _ExpandableFabState extends State<ExpandableFab>
+class ExpandableFabState extends State<ExpandableFab>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
   late final Animation<double> _expandAnimation;
