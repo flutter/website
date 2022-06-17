de4d9c5f6354184ad4557536ef252d3b7f9eabb5
Ross Llewallyn <rossllewallyn@protonmail.com>
2022-06-17 11:25:05 -0400

    keep state class private; fix createState

--- examples/cookbook/effects/expandable_fab/lib/main.dart
+++ examples/cookbook/effects/expandable_fab/lib/main.dart
@@ -84,10 +84,10 @@ class ExpandableFab extends StatefulWidget {
   final List<Widget> children;
 
   @override
-  ExpandableFabState createState() => ExpandableFabState();
+  State<ExpandableFab> createState() => _ExpandableFabState();
 }
 
-class ExpandableFabState extends State<ExpandableFab>
+class _ExpandableFabState extends State<ExpandableFab>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
   late final Animation<double> _expandAnimation;
