e84762307ec0350b6859200d7de778bb6095ff84
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-07-30 20:42:47 +0545

    fixed chrome unresponsive issue

--- examples/cookbook/lists/long_lists/lib/main.dart
+++ examples/cookbook/lists/long_lists/lib/main.dart
@@ -28,6 +28,11 @@ class MyApp extends StatelessWidget {
         // #docregion ListView
         body: ListView.builder(
           itemCount: items.length,
+          //when the listview is scrolled using the scrollbar all the way to the bottom of the view,
+          //chrome gets unresponsive
+          // specifying prototypeItem fixes the issue because the scrolling
+          //machinery can make use of the foreknowledge of the children's extent
+          prototypeItem: const ListTile(title: Text('prototype item')),
           itemBuilder: (context, index) {
             return ListTile(
               title: Text(items[index]),
