7cccff82df91b3fc47ebc1b1b5b02c82d31b18cc
Leigha Jarett <leigha.jarett@gmail.com>
2022-11-08 17:42:00 -0500

    accidental typo

--- examples/get-started/flutter-for/ios_devs/lib/column.dart
+++ examples/get-started/flutter-for/ios_devs/lib/column.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoApp(pub
+    return const CupertinoApp(
       home: HomePage(),
     );
   }
