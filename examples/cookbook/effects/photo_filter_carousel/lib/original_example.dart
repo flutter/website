5408e776d03238aeef374090d3219a5da826c748
Parker Lougheed <parlough@gmail.com>
2022-12-15 23:02:59 -0600

    Standardize lower SDK constraint to a released version (#7950)

--- examples/cookbook/effects/photo_filter_carousel/lib/original_example.dart
+++ examples/cookbook/effects/photo_filter_carousel/lib/original_example.dart
@@ -56,8 +56,7 @@ class _ExampleInstagramFilterSelectionState
   Widget _buildPhotoWithFilter() {
     return ValueListenableBuilder(
       valueListenable: _filterColor,
-      builder: (context, value, child) {
-        final color = value as Color;
+      builder: (context, color, child) {
         return Image.network(
           'https://flutter.dev/docs/cookbook/img-files/effects/instagram-buttons/millenial-dude.jpg',
           color: color.withOpacity(0.5),
