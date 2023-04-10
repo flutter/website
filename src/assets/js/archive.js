5a69abbe005b3c72260c1cf901a932e2b11a6d40
Jesse Seales <jseales@google.com>
2023-04-10 15:12:16 -0400

    update provenance formatting

--- src/assets/js/archive.js
+++ src/assets/js/archive.js
@@ -181,7 +181,7 @@ function getProvenanceLink(os, release, date, channel) {
   return $("<a />").attr("href", 
     `${baseUrl}${channel}/${os}/flutter_${os}_${release.version}-${channel}`+
     `.zip.intoto.jsonl`
-  ).text('View provenance')
+  ).text('View')
 }
 
 // Send requests to render the tables.
