5f13763ac2fbcfd7bb97e62f8a925117502d192e
tasospal <apalaiop@csd.auth.gr>
2023-06-02 19:45:46 +0300

    bad-smells corection (second commit)

--- src/assets/js/go_link_index.js
+++ src/assets/js/go_link_index.js
@@ -1,62 +1,69 @@
-// Fetches Go link redirects from firebase.json
-// Since we can't access it from the site directly, fetch it from GitHub main.
-function fetchGoLinks(callback, errorCallback, goLinksElement) {
-  const firebaseConfigUrl = 'https://raw.githubusercontent.com/flutter/website/main/firebase.json';
+function fetchGoLinks() {
+    const firebaseConfigUrl = 'https://raw.githubusercontent.com/flutter/website/main/firebase.json';
 
-  fetch(firebaseConfigUrl)
-      .then(response => {
-        if (response.status === 200) {
-          return response.json();
-        } else {
-          throw Error('Failed to load resources.');
-        }
-      })
-      .then(firebaseConfig => updateList(firebaseConfig.hosting.redirects, goLinksElement))
-      .catch(_ => {
-        if (errorCallback) errorCallback(goLinksElement);
-      });
+    return fetch(firebaseConfigUrl)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Failed to load resources.');
+            }
+            return response.json();
+        })
+        .then((firebaseConfig) => firebaseConfig.hosting.redirects)
+        .catch((error) => {
+            console.error('Error fetching Go links:', error);
+            throw error; // Propagate the error to handle it at a higher level
+        });
 }
 
-function updateList(redirects, goLinksElement) {
-  redirects.filter(
-      redirect =>
-          redirect.source &&
-          redirect.source.startsWith('/go/') &&
-          redirect.source !== '/go/template' &&
-          redirect.destination.startsWith('https://docs.google.com/')
-  ).forEach(
-      redirect => {
-        const anchorElement = document.createElement('a');
-        anchorElement.href = redirect.destination;
-        anchorElement.textContent = redirect.source;
+function createGoLinkElements(redirects) {
+    const fragment = document.createDocumentFragment();
 
-        const listItemElement = document.createElement('li');
-        listItemElement.appendChild(anchorElement);
+    redirects
+        .filter(
+            (redirect) =>
+                redirect.source &&
+                redirect.source.startsWith('/go/') &&
+                redirect.source !== '/go/template' &&
+                redirect.destination.startsWith('https://docs.google.com/')
+        )
+        .forEach((redirect) => {
+            const anchorElement = document.createElement('a');
+            anchorElement.href = redirect.destination;
+            anchorElement.textContent = redirect.source;
 
-        goLinksElement.appendChild(listItemElement);
-      }
-  );
+            const listItemElement = document.createElement('li');
+            listItemElement.appendChild(anchorElement);
+
+            fragment.appendChild(listItemElement);
+        });
+
+    return fragment;
+}
+
+function updateList(goLinksElement, redirects) {
+    const goLinkElements = createGoLinkElements(redirects);
+    goLinksElement.appendChild(goLinkElements);
 }
 
 function updateListFailed(goLinksElement) {
-  const failedElement = document.createElement('li');
-  failedElement.textContent = 'Failed to load resources. Refresh page to try again.';
-
-  goLinksElement.appendChild(failedElement);
+    const failedElement = document.createElement('li');
+    failedElement.textContent = 'Failed to load resources. Refresh page to try again.';
+    goLinksElement.appendChild(failedElement);
 }
 
 function setup() {
-  const goLinksElement = document.getElementById('go-links');
-  if (!goLinksElement) {
-    return;
-  }
+    const goLinksElement = document.getElementById('go-links');
+    if (!goLinksElement) {
+        return;
+    }
 
-  // Send request to get list, then render the list
-  fetchGoLinks(updateList, updateListFailed, goLinksElement);
+    fetchGoLinks()
+        .then((redirects) => updateList(goLinksElement, redirects))
+        .catch(() => updateListFailed(goLinksElement));
 }
 
-if (document.readyState !== "loading") {
-  setup();
+if (document.readyState !== 'loading') {
+    setup();
 } else {
-  document.addEventListener("DOMContentLoaded", setup);
+    document.addEventListener('DOMContentLoaded', setup);
 }
