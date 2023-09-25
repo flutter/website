871cb9799384e69ed8edf97d1c0d89938d5fbe04
esodot <alexander.gerk@zeppelin.com>
2023-09-25 23:20:23 +0200

    Update ci_post_clone.sh for new xcloud_cloud variable (#9441)
    Update deprecated ENV variable

--- examples/deployment/xcode_cloud/ci_post_clone.sh
+++ examples/deployment/xcode_cloud/ci_post_clone.sh
@@ -4,7 +4,7 @@
 set -e
 
 # The default execution directory of this script is the ci_scripts directory.
-cd $CI_WORKSPACE # change working directory to the root of your cloned repo.
+cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
