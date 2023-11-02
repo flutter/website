3990fc362ee780ed709dc962ad466aad9e8f973b
Drew Roen <drewroen@google.com>
2023-11-02 17:16:18 -0500

    Refactor staging

--- cloud_build/stage_site_and_comment_on_github.sh
+++ cloud_build/stage_site_and_comment_on_github.sh
@@ -18,21 +18,14 @@ login_to_github() {
 
 comment_staging_url_on_github () {
     echo "Commenting staging url on the PR..."
-    echo -e "Visit the preview URL for this PR (updated for commit $COMMIT_SHA):\n\n$FIREBASE_STAGING_URL" > PR_BODY
+    COMMENT_BODY=$(echo -e "Visit the preview URL for this PR (updated for commit $COMMIT_SHA):\n\n$FIREBASE_STAGING_URL")
 
     # The github CLI throws an error if --edit-last doesn't find a previous
     # comment, so this edits the last comment, but if it doesn't exist,
     # leave a new comment.
-    gh pr comment $PR_NUMBER --edit-last --body-file PR_BODY --repo $REPO_FULL_NAME || \
-        gh pr comment $PR_NUMBER ---body-file PR_BODY --repo $REPO_FULL_NAME
+    gh pr comment $PR_NUMBER --edit-last --body "$COMMENT_BODY" --repo $REPO_FULL_NAME || \
+        gh pr comment $PR_NUMBER --body "$COMMENT_BODY" --repo $REPO_FULL_NAME
 }
-
-PR_NUMBER=$1
-HEAD_BRANCH=$2
-PROJECT_ID=$3
-COMMIT_SHA=$4
-REPO_FULL_NAME=$5
-GH_PAT_TOKEN=$6
 
 deploy_to_firebase_staging_channel
 login_to_github
