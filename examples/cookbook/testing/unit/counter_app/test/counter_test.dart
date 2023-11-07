faeb150f7ebf644ddd7c06172091054a834e8ca0
Anthony Sansone <atsansone@users.noreply.github.com>
2023-11-06 21:58:16 -0600

    Update counter_test.dart

--- examples/cookbook/testing/unit/counter_app/test/counter_test.dart
+++ examples/cookbook/testing/unit/counter_app/test/counter_test.dart
@@ -5,7 +5,9 @@ import 'package:test/test.dart';
 void main() {
   test('Counter value should be incremented', () {
     final counter = Counter();
+    
     counter.increment();
+    
     expect(counter.value, 1);
   });
 }
