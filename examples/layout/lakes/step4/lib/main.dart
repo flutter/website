45790892a4b72e40ad79a207dcdf7f301d78d681
Parker Lougheed <parlough@gmail.com>
2023-12-13 21:40:57 -0600

    Revert "Update layout tutorial for current best practices" (#9927)

--- examples/layout/lakes/step4/lib/main.dart
+++ examples/layout/lakes/step4/lib/main.dart
@@ -2,63 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
-// #docregion TextAdd
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-// #enddocregion TextAdd
-    const String appTitle = 'Flutter layout demo';
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        // #docregion addWidget
-        // #docregion TextAdd
-        body: const SingleChildScrollView(
-          child: Column(
-            children: [
-              TitleSection(
-                name: 'Oeschinen Lake Campground',
-                location: 'Kandersteg, Switzerland',
-              ),
-              ButtonSection(),
-              TextSection(
-                description:
-                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
-                    'Bernese Alps. Situated 1,578 meters above sea level, it '
-                    'is one of the larger Alpine Lakes. A gondola ride from '
-                    'Kandersteg, followed by a half-hour walk through pastures '
-                    'and pine forest, leads you to the lake, which warms to 20 '
-                    'degrees Celsius in the summer. Activities enjoyed here '
-                    'include rowing, and riding the summer toboggan run.',
-              ),
-            ],
-          ),
-        ),
-        // #enddocregion addWidget
-      ),
-    );
-  }
-}
-// #enddocregion TextAdd
-
-class TitleSection extends StatelessWidget {
-  const TitleSection({
-    super.key,
-    required this.name,
-    required this.location,
-  });
-
-  final String name;
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
+    Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
         children: [
@@ -68,17 +17,17 @@ class TitleSection extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 /*2*/
-                Padding(
+                Container(
                   padding: const EdgeInsets.only(bottom: 8),
-                  child: Text(
-                    name,
-                    style: const TextStyle(
+                  child: const Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
                 Text(
-                  location,
+                  'Kandersteg, Switzerland',
                   style: TextStyle(
                     color: Colors.grey[500],
                   ),
@@ -95,61 +44,58 @@ class TitleSection extends StatelessWidget {
         ],
       ),
     );
-  }
-}
 
-class ButtonSection extends StatelessWidget {
-  const ButtonSection({super.key});
+    Color color = Theme.of(context).primaryColor;
 
-  @override
-  Widget build(BuildContext context) {
-    final Color color = Theme.of(context).primaryColor;
-    return SizedBox(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          BuildButtonColumn(
-            color: color,
-            icon: Icons.call,
-            label: 'CALL',
-          ),
-          BuildButtonColumn(
-            color: color,
-            icon: Icons.near_me,
-            label: 'ROUTE',
-          ),
-          BuildButtonColumn(
-            color: color,
-            icon: Icons.share,
-            label: 'SHARE',
-          ),
-        ],
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    // #docregion textSection
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+    // #enddocregion textSection
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
       ),
     );
   }
-}
 
-class BuildButtonColumn extends StatelessWidget {
-  const BuildButtonColumn({
-    super.key,
-    required this.color,
-    required this.icon,
-    required this.label,
-  });
-
-  final Color color;
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Icon(icon, color: color),
-        Padding(
-          padding: const EdgeInsets.only(top: 8),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
           child: Text(
             label,
             style: TextStyle(
@@ -163,25 +109,3 @@ class BuildButtonColumn extends StatelessWidget {
     );
   }
 }
-
-// #docregion TextSection
-class TextSection extends StatelessWidget {
-  const TextSection({
-    super.key,
-    required this.description,
-  });
-
-  final String description;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        description,
-        softWrap: true,
-      ),
-    );
-  }
-}
-// #enddocregion TextSection
--- src/ui/interactivity/index.md
+++ src/ui/interactivity/index.md
@@ -274,89 +274,10 @@ In the same location, create the stateful widget:
 ```diff
 --- layout/lakes/step6/lib/main.dart
 +++ layout/lakes/interactive/lib/main.dart
-@@ -10,9 +5,57 @@
+@@ -10,2 +5,2 @@
  class MyApp extends StatelessWidget {
    const MyApp({super.key});
-
-   @override
-   Widget build(BuildContext context) {
--    Widget titleSection = Container(
-+    const String appTitle = 'Flutter layout demo';
-+    return MaterialApp(
-+      title: appTitle,
-+      home: Scaffold(
-+        appBar: AppBar(
-+          title: const Text(appTitle),
-+        ),
-+        body: const SingleChildScrollView(
-+          child: Column(
-+            children: [
-+              ImageSection(
-+                image: 'images/lake.jpg',
-+              ),
-+              TitleSection(
-+                name: 'Oeschinen Lake Campground',
-+                location: 'Kandersteg, Switzerland',
-+              ),
-+              ButtonSection(),
-+              TextSection(
-+                description:
-+                    'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
-+                    'Bernese Alps. Situated 1,578 meters above sea level, it '
-+                    'is one of the larger Alpine Lakes. A gondola ride from '
-+                    'Kandersteg, followed by a half-hour walk through pastures '
-+                    'and pine forest, leads you to the lake, which warms to 20 '
-+                    'degrees Celsius in the summer. Activities enjoyed here '
-+                    'include rowing, and riding the summer toboggan run.',
-+              ),
-+            ],
-+          ),
-+        ),
-+      ),
-+    );
-+  }
-+}
-+
-+class TitleSection extends StatelessWidget {
-+  const TitleSection({
-+    super.key,
-+    required this.name,
-+    required this.location,
-+  });
-+
-+  final String name;
-+  final String location;
-+
-+  @override
-+  Widget build(BuildContext context) {
-+    return Padding(
-       padding: const EdgeInsets.all(32),
-       child: Row(
-         children: [
-@@ -22,17 +65,17 @@
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [
-                 /*2*/
--                Container(
-+                Padding(
-                   padding: const EdgeInsets.only(bottom: 8),
--                  child: const Text(
--                    'Oeschinen Lake Campground',
--                    style: TextStyle(
-+                  child: Text(
-+                    name,
-+                    style: const TextStyle(
-                       fontWeight: FontWeight.bold,
-                     ),
-                   ),
-                 ),
-                 Text(
--                  'Kandersteg, Switzerland',
-+                  location,
-                   style: TextStyle(
-                     color: Colors.grey[500],
-                   ),
-@@ -40,11 +83,7 @@
+@@ -40,11 +35,7 @@
                ],
              ),
            ),
