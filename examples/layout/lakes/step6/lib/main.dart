706d6771757dbad3c22888764bb44add5a060353
Tony Sansone <atsansone@google.com>
2023-12-19 17:32:35 -0600

    Fixed code excerpts

--- examples/layout/lakes/step6/lib/main.dart
+++ examples/layout/lakes/step6/lib/main.dart
@@ -109,17 +109,17 @@ class ButtonSection extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          BuildButtonColumn(
+          ButtonWithText(
             color: color,
             icon: Icons.call,
             label: 'CALL',
           ),
-          BuildButtonColumn(
+          ButtonWithText(
             color: color,
             icon: Icons.near_me,
             label: 'ROUTE',
           ),
-          BuildButtonColumn(
+          ButtonWithText(
             color: color,
             icon: Icons.share,
             label: 'SHARE',
@@ -130,8 +130,8 @@ class ButtonSection extends StatelessWidget {
   }
 }
 
-class BuildButtonColumn extends StatelessWidget {
-  const BuildButtonColumn({
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
     super.key,
     required this.color,
     required this.icon,
@@ -204,3 +204,65 @@ class ImageSection extends StatelessWidget {
   }
 }
 // #enddocregion ImageSection
+
+// #docregion FavoriteWidget
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
+
+  @override
+  State<FavoriteWidget> createState() => _FavoriteWidgetState();
+}
+// #enddocregion FavoriteWidget
+
+// #docregion _FavoriteWidgetState, _FavoriteWidgetState-fields, _FavoriteWidgetState-build
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  // #enddocregion _FavoriteWidgetState-build
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  // #enddocregion _FavoriteWidgetState-fields
+
+  // #docregion _toggleFavorite
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
+  // #enddocregion _toggleFavorite
+
+  // #docregion _FavoriteWidgetState-build
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+                ? const Icon(Icons.star)
+                : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+// #docregion _FavoriteWidgetState-fields
+}
