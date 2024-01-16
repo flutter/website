dabbf1ec16b688e6d38ae14bbf6968d83d297516
Parker Lougheed <parlough@gmail.com>
2024-01-16 14:19:27 -0600

    Fix video embed on 'Common errors' page and nearby cleanup (#10050)

--- examples/testing/common_errors/lib/renderflex_overflow.dart
+++ examples/testing/common_errors/lib/renderflex_overflow.dart
@@ -15,11 +15,12 @@ class ProblemWidget extends StatelessWidget {
           children: [
             Text('Title', style: Theme.of(context).textTheme.headlineMedium),
             const Text(
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'
-                ' do eiusmod tempor incididunt ut labore et dolore magna '
-                'aliqua. Ut enim ad minim veniam, quis nostrud '
-                'exercitation ullamco laboris nisi ut aliquip ex ea '
-                'commodo consequat.'),
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed '
+              'do eiusmod tempor incididunt ut labore et dolore magna '
+              'aliqua. Ut enim ad minim veniam, quis nostrud '
+              'exercitation ullamco laboris nisi ut aliquip ex ea '
+              'commodo consequat.',
+            ),
           ],
         ),
       ],
