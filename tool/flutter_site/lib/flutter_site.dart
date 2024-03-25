12487e4b58d89d393c55a8ca72c586d3fd59abab
Parker Lougheed <parlough@gmail.com>
2024-03-25 22:06:49 +0100

    Add root dash_site command for consistency with dart.dev (#10333)
    This command will be used as an easy entry point into site tooling while
    also verifying the user's setup and conveying any issues.
    
    Instructions for use will come in follow-up PRs.
    
    Contributes to https://github.com/flutter/website/issues/10203

--- tool/flutter_site/lib/flutter_site.dart
+++ tool/flutter_site/lib/flutter_site.dart
@@ -5,11 +5,13 @@
 import 'package:args/command_runner.dart';
 
 import 'src/commands/analyze_dart.dart';
+import 'src/commands/build.dart';
 import 'src/commands/check_all.dart';
 import 'src/commands/check_link_references.dart';
 import 'src/commands/check_links.dart';
 import 'src/commands/format_dart.dart';
 import 'src/commands/refresh_excerpts.dart';
+import 'src/commands/serve.dart';
 import 'src/commands/test_dart.dart';
 import 'src/commands/verify_firebase_json.dart';
 
@@ -22,13 +24,15 @@ final class FlutterSiteCommandRunner extends CommandRunner<int> {
           'flutter_site',
           'Infrastructure tooling for the Flutter documentation website.',
         ) {
+    addCommand(AnalyzeDartCommand());
+    addCommand(BuildSiteCommand());
+    addCommand(CheckAllCommand());
     addCommand(CheckLinksCommand());
     addCommand(CheckLinkReferencesCommand());
-    addCommand(VerifyFirebaseJsonCommand());
-    addCommand(RefreshExcerptsCommand());
     addCommand(FormatDartCommand());
-    addCommand(AnalyzeDartCommand());
+    addCommand(RefreshExcerptsCommand());
+    addCommand(ServeSiteCommand());
     addCommand(TestDartCommand());
-    addCommand(CheckAllCommand());
+    addCommand(VerifyFirebaseJsonCommand());
   }
 }
---
+++ tool/flutter_site/lib/src/commands/build.dart
@@ -0,0 +1,30 @@
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:args/command_runner.dart';
+
+final class BuildSiteCommand extends Command<int> {
+  static const String _releaseFlag = 'release';
+
+  BuildSiteCommand() {
+    argParser.addFlag(
+      _releaseFlag,
+      defaultsTo: false,
+      help: 'Build a release build for docs.flutter.dev. '
+          'Optimizes site resources.',
+    );
+  }
+
+  @override
+  String get description => 'Build the site.';
+
+  @override
+  String get name => 'build';
+
+  @override
+  Future<int> run() async {
+    print('TODO: Not yet implemented.');
+    return 0;
+  }
+}
