328af76edbe6d33cdcc3d6e57a106ee529b5dd7e
Tirth <pateltirth454@gmail.com>
2024-08-07 21:54:42 +0530

    Update canvas.dart

--- examples/get-started/flutter-for/ios_devs/lib/canvas.dart
+++ examples/get-started/flutter-for/ios_devs/lib/canvas.dart
@@ -31,8 +31,8 @@ class SignatureState extends State<Signature> {
       },
       onPanEnd: (details) => _points.add(null),
       child:
-          // #docregion custom-paint
-          CustomPaint(
+      // #docregion custom-paint
+      CustomPaint(
         painter: SignaturePainter(_points),
         size: Size.infinite,
       ),
