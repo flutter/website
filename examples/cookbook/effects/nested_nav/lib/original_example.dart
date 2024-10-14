ad813d85500af8ca1faaee0bed9dfe8847d80bfc
Parker Lougheed <parlough@gmail.com>
2024-10-13 22:37:24 -0500

    Add back SingleChildScrollView and regenerate

--- examples/cookbook/effects/nested_nav/lib/original_example.dart
+++ examples/cookbook/effects/nested_nav/lib/original_example.dart
@@ -298,56 +298,58 @@ class FinishedPage extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: 250,
-                height: 250,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xFF222222),
-                ),
-                child: const Center(
-                  child: Icon(
-                    Icons.lightbulb,
-                    size: 175,
-                    color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  width: 250,
+                  height: 250,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFF222222),
+                  ),
+                  child: const Center(
+                    child: Icon(
+                      Icons.lightbulb,
+                      size: 175,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              const Text(
-                'Bulb added!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                style: ButtonStyle(
-                  padding: WidgetStateProperty.resolveWith((states) {
-                    return const EdgeInsets.symmetric(
-                        horizontal: 24, vertical: 12);
-                  }),
-                  backgroundColor: WidgetStateColor.resolveWith((states) {
-                    return const Color(0xFF222222);
-                  }),
-                  shape: WidgetStateProperty.resolveWith((states) {
-                    return const StadiumBorder();
-                  }),
-                ),
-                onPressed: onFinishPressed,
-                child: const Text(
-                  'Finish',
+                const SizedBox(height: 32),
+                const Text(
+                  'Bulb added!',
+                  textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 24,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 32),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    padding: WidgetStateProperty.resolveWith((states) {
+                      return const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12);
+                    }),
+                    backgroundColor: WidgetStateColor.resolveWith((states) {
+                      return const Color(0xFF222222);
+                    }),
+                    shape: WidgetStateProperty.resolveWith((states) {
+                      return const StadiumBorder();
+                    }),
+                  ),
+                  onPressed: onFinishPressed,
+                  child: const Text(
+                    'Finish',
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
