d2583cac5f01163658debc976812872ac00cbcd8
Miguel Beltran <m@beltran.work>
2024-11-22 18:20:46 +0100

    Add "Offline-first" architecture cookbook recipe (#11425)
    _Description of what this PR is changing or adding, and why:_
    
    Following PRs #11410 and #11394 this PR adds the "Offline-first"
    cookbook recipe for the Architecture design patterns.
    
    _Issues fixed by this PR (if any):_
    
    Part of https://github.com/flutter/website/issues/11374
    
    _PRs or commits this PR depends on (if any):_
    
    ## Presubmit checklist
    
    - [ ] This PR is marked as draft with an explanation if not meant to
    land until a future stable release.
    - [x] This PR doesn’t contain automatically generated corrections
    (Grammarly or similar).
    - [x] This PR follows the [Google Developer Documentation Style
    Guidelines](https://developers.google.com/style) — for example, it
    doesn’t use _i.e._ or _e.g._, and it avoids _I_ and _we_ (first person).
    - [x] This PR uses [semantic line
    breaks](https://github.com/dart-lang/site-shared/blob/main/doc/writing-for-dart-and-flutter-websites.md#semantic-line-breaks)
    of 80 characters or fewer.
    
    ---------
    
    Co-authored-by: Shams Zakhour (ignore Sfshaza) <44418985+sfshaza2@users.noreply.github.com>

---
+++ examples/cookbook/architecture/offline_first/lib/domain/model/user_profile.freezed.dart
@@ -0,0 +1,182 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'user_profile.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$UserProfile {
+  String get name => throw _privateConstructorUsedError;
+  String get photoUrl => throw _privateConstructorUsedError;
+  bool get synchronized => throw _privateConstructorUsedError;
+
+  /// Create a copy of UserProfile
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  $UserProfileCopyWith<UserProfile> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $UserProfileCopyWith<$Res> {
+  factory $UserProfileCopyWith(
+          UserProfile value, $Res Function(UserProfile) then) =
+      _$UserProfileCopyWithImpl<$Res, UserProfile>;
+  @useResult
+  $Res call({String name, String photoUrl, bool synchronized});
+}
+
+/// @nodoc
+class _$UserProfileCopyWithImpl<$Res, $Val extends UserProfile>
+    implements $UserProfileCopyWith<$Res> {
+  _$UserProfileCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of UserProfile
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? photoUrl = null,
+    Object? synchronized = null,
+  }) {
+    return _then(_value.copyWith(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      photoUrl: null == photoUrl
+          ? _value.photoUrl
+          : photoUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      synchronized: null == synchronized
+          ? _value.synchronized
+          : synchronized // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$UserProfileImplCopyWith<$Res>
+    implements $UserProfileCopyWith<$Res> {
+  factory _$$UserProfileImplCopyWith(
+          _$UserProfileImpl value, $Res Function(_$UserProfileImpl) then) =
+      __$$UserProfileImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String name, String photoUrl, bool synchronized});
+}
+
+/// @nodoc
+class __$$UserProfileImplCopyWithImpl<$Res>
+    extends _$UserProfileCopyWithImpl<$Res, _$UserProfileImpl>
+    implements _$$UserProfileImplCopyWith<$Res> {
+  __$$UserProfileImplCopyWithImpl(
+      _$UserProfileImpl _value, $Res Function(_$UserProfileImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of UserProfile
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? name = null,
+    Object? photoUrl = null,
+    Object? synchronized = null,
+  }) {
+    return _then(_$UserProfileImpl(
+      name: null == name
+          ? _value.name
+          : name // ignore: cast_nullable_to_non_nullable
+              as String,
+      photoUrl: null == photoUrl
+          ? _value.photoUrl
+          : photoUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      synchronized: null == synchronized
+          ? _value.synchronized
+          : synchronized // ignore: cast_nullable_to_non_nullable
+              as bool,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$UserProfileImpl implements _UserProfile {
+  const _$UserProfileImpl(
+      {required this.name, required this.photoUrl, this.synchronized = false});
+
+  @override
+  final String name;
+  @override
+  final String photoUrl;
+  @override
+  @JsonKey()
+  final bool synchronized;
+
+  @override
+  String toString() {
+    return 'UserProfile(name: $name, photoUrl: $photoUrl, synchronized: $synchronized)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$UserProfileImpl &&
+            (identical(other.name, name) || other.name == name) &&
+            (identical(other.photoUrl, photoUrl) ||
+                other.photoUrl == photoUrl) &&
+            (identical(other.synchronized, synchronized) ||
+                other.synchronized == synchronized));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, name, photoUrl, synchronized);
+
+  /// Create a copy of UserProfile
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$UserProfileImplCopyWith<_$UserProfileImpl> get copyWith =>
+      __$$UserProfileImplCopyWithImpl<_$UserProfileImpl>(this, _$identity);
+}
+
+abstract class _UserProfile implements UserProfile {
+  const factory _UserProfile(
+      {required final String name,
+      required final String photoUrl,
+      final bool synchronized}) = _$UserProfileImpl;
+
+  @override
+  String get name;
+  @override
+  String get photoUrl;
+  @override
+  bool get synchronized;
+
+  /// Create a copy of UserProfile
+  /// with the given fields replaced by the non-null parameter values.
+  @override
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$UserProfileImplCopyWith<_$UserProfileImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
