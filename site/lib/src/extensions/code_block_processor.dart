1448e24f3aafaa25eae0f71abe5f9edc62df19eb
Kilian Schulte <netlight@schultek.dev>
2025-10-17 15:52:21 +0200

    fix missing run property

--- site/lib/src/extensions/code_block_processor.dart
+++ site/lib/src/extensions/code_block_processor.dart
@@ -70,6 +70,7 @@ final class CodeBlockProcessor implements PageExtension {
                 title: title ?? 'Runnable Flutter example',
                 theme: metadata['theme'],
                 height: metadata['height'],
+                runAutomatically: metadata['run'] == 'true',
               ),
             );
           }
