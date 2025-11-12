c503543a80300f464981a51c49a7c2384ad5d0d4
Parker Lougheed <parlough@gmail.com>
2025-11-12 12:25:37 -0600

    Switch to a more flexible banner format

--- site/lib/src/components/layout/banner.dart
+++ site/lib/src/components/layout/banner.dart
@@ -4,43 +4,11 @@
 
 import 'package:jaspr/jaspr.dart';
 
-/// The information to display in the site banner,
-/// as configured in the `src/data/banner.yml` file.
-@immutable
-final class BannerContent {
-  final String text;
-  final String linkText;
-  final String linkUri;
-  final bool newTab;
-
-  const BannerContent({
-    required this.text,
-    required this.linkText,
-    required this.linkUri,
-    this.newTab = false,
-  });
-
-  factory BannerContent.fromMap(Map<String, Object?> bannerData) {
-    final text = bannerData['text'] as String;
-    final link = bannerData['link'] as Map<Object?, Object?>;
-    final linkText = link['text'] as String;
-    final linkUri = link['url'] as String;
-    final newTab = link['newTab'] as bool? ?? false;
-
-    return BannerContent(
-      text: text,
-      linkText: linkText,
-      linkUri: linkUri,
-      newTab: newTab,
-    );
-  }
-}
-
 /// The site-wide banner.
 class DashBanner extends StatelessComponent {
-  const DashBanner(this.content, {super.key});
+  const DashBanner(this.inlineHtmlContent, {super.key});
 
-  final BannerContent content;
+  final String inlineHtmlContent;
 
   @override
   Component build(BuildContext context) => div(
@@ -48,13 +16,7 @@ class DashBanner extends StatelessComponent {
     attributes: {'role': 'alert'},
     [
       p([
-        text(content.text),
-        text(' '),
-        a(
-          href: content.linkUri,
-          target: content.newTab ? Target.blank : null,
-          [text(content.linkText)],
-        ),
+        raw(inlineHtmlContent),
       ]),
     ],
   );
