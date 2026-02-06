a51eed0eae298d7e30586040cf4ef3777955ca12
Parker Lougheed <parlough@gmail.com>
2026-02-06 14:35:01 +0800

    Fix sitemap date formatting to fix build

--- site/lib/src/loaders/data_processor.dart
+++ site/lib/src/loaders/data_processor.dart
@@ -90,5 +90,10 @@ final Map<String, DateTime> _lastModifiedPerPath = () {
 }();
 
 extension on DateTime {
-  String get formatted => '$year-$month-$day';
+  String get formatted {
+    final year = this.year.toString().padLeft(4, '0');
+    final month = this.month.toString().padLeft(2, '0');
+    final day = this.day.toString().padLeft(2, '0');
+    return '$year-$month-$day';
+  }
 }
