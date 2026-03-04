712934a7de7f0b3cea901cfe22e00ff922ad67bb
lamek <kevin.lamenzo@gmail.com>
2026-03-04 02:03:59 +0000

    Suppress pedagogy analyzer warnings in snapshot files

--- examples/fwe/birdle/lib/step4k_main.dart
+++ examples/fwe/birdle/lib/step4k_main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: unused_import, unused_field, unused_local_variable, avoid_print, prefer_const_constructors_in_immutables, use_key_in_widget_constructors, sized_box_for_whitespace
 import 'package:flutter/material.dart';
 import 'game.dart';
 import 'step2_main.dart' show Tile;
