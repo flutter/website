cc422d4504e3007c1a95a8be7e8f4c39a5a68748
Kevin Lamenzo <kevin.lamenzo@gmail.com>
2026-04-18 12:55:36 -0700

    Update FWE tutorial snippets Pt 3 (#13271)
    Updated code for FWE pt3 and moves code excerpts to the `/examples` directory.
    
    ---------
    
    Co-authored-by: Parker Lougheed <parlough@gmail.com>

---
+++ examples/fwe/rolodex/lib/step3_slivers/screens/contact_groups_v1.dart
@@ -0,0 +1,60 @@
+// #docregion contact_groups_view
+import 'package:flutter/cupertino.dart';
+
+import '../data/contact_group.dart';
+import '../main.dart';
+
+// #docregion contact_groups_page
+class ContactGroupsPage extends StatelessWidget {
+  const ContactGroupsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return _ContactGroupsView(
+      selectedListId: 0,
+      onListSelected: (list) {
+        debugPrint(list.toString());
+      },
+    );
+  }
+}
+// #enddocregion contact_groups_page
+
+// #docregion contact_groups_view
+class _ContactGroupsView extends StatelessWidget {
+  const _ContactGroupsView({required this.onListSelected, this.selectedListId});
+
+  final int? selectedListId;
+  final Function(ContactGroup) onListSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      backgroundColor: CupertinoColors.extraLightBackgroundGray,
+      child: CustomScrollView(
+        slivers: [
+          const CupertinoSliverNavigationBar(largeTitle: Text('Lists')),
+          SliverFillRemaining(
+            child: ValueListenableBuilder<List<ContactGroup>>(
+              valueListenable: contactGroupsModel.listsNotifier,
+              builder: (context, contactLists, child) {
+                return CupertinoListSection.insetGrouped(
+                  header: const Text('iPhone'),
+                  children: [
+                    for (final ContactGroup contactList in contactLists)
+                      CupertinoListTile(
+                        title: Text(contactList.label),
+                        onTap: () => onListSelected(contactList),
+                      ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// #enddocregion contact_groups_view
