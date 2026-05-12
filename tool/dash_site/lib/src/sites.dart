c3cbb856246649b4c8a910d7c180425dff7f3f21
Parker Lougheed <parlough@gmail.com>
2026-05-12 16:34:52 -0500

    Add support for the site from the dash_site tool

--- tool/dash_site/lib/src/sites.dart
+++ tool/dash_site/lib/src/sites.dart
@@ -8,15 +8,36 @@ import 'package:path/path.dart' as path;
 /// The sites maintained in this repository.
 enum Site {
   /// The Flutter documentation site.
-  docs(baseUrl: 'https://docs.flutter.dev');
+  docs(
+    baseUrl: 'https://docs.flutter.dev',
+    contentPathSegments: ['src', 'content'],
+    supportsCodeExcerpts: true,
+  ),
 
-  const Site({required this.baseUrl});
+  /// The Flutter marketing site.
+  www(baseUrl: 'https://flutter.dev', contentPathSegments: ['content']);
+
+  const Site({
+    required this.baseUrl,
+    required this.contentPathSegments,
+    this.supportsCodeExcerpts = false,
+  });
 
   /// The canonical base URL where this site is hosted in production.
   final String baseUrl;
 
+  /// The path segments for this site's content directory.
+  final List<String> contentPathSegments;
+
+  /// Whether this site supports code excerpts managed by `refresh-excerpts`.
+  final bool supportsCodeExcerpts;
+
   /// The directory where this site's content and implementation is located.
   String get directory => path.join('sites', name);
+
+  /// The directory where this site's content is located.
+  String get contentDirectory =>
+      path.joinAll([directory, ...contentPathSegments]);
 }
 
 /// The name of the global `--site` option.
