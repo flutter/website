bd1d3f233f9f764eb3ede79457c9075e4c442543
Parker Lougheed <parlough@gmail.com>
2026-06-14 18:38:00 +0200

    Move slugify to src directory

--- packages/site_shared/lib/util.dart
+++ packages/site_shared/lib/util.dart
@@ -2,26 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'src/utils/slugify.dart';
+
 /// Whether this build of the site will be deployed to production.
-const productionBuild = bool.fromEnvironment('PRODUCTION');
-
-/// Converts the specified [text] into a standardized URL slug
-/// that can be used as the ID for headers and other anchors in HTML.
-String slugify(String text) => text
-    .toLowerCase()
-    .trim()
-    .replaceAll(_slugifyPunctuationToReplace, '-')
-    .replaceAll(_slugifyUnsupportedToRemove, '')
-    .replaceAll(_slugifyCharsToCombine, '-')
-    .replaceAll(_slugifyHyphenTrim, '');
-
-final RegExp _slugifyPunctuationToReplace = RegExp(r'[:._]');
-final RegExp _slugifyUnsupportedToRemove = RegExp(
-  r'[^\p{L}\p{N}\s:.-]',
-  unicode: true,
-);
-final RegExp _slugifyCharsToCombine = RegExp(r'[\s-]+');
-final RegExp _slugifyHyphenTrim = RegExp(r'^-+|-+$');
+const bool productionBuild = .fromEnvironment('PRODUCTION');
 
 final RegExp _attributePattern = RegExp(r'(\w+)="([^"]*)"');
 final RegExp _whitespacePattern = RegExp(r'\s+');
