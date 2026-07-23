71f660bb4d27f8cca41184d0f5144647502dd67c
Parker Lougheed <parlough@gmail.com>
2026-07-23 17:05:12 -0500

    Update some license headers for moved files

--- packages/site_shared/lib/components/blog/blog_next_posts.dart
+++ packages/site_shared/lib/components/blog/blog_next_posts.dart
@@ -1,6 +1,7 @@
-// Copyright (c) 2026, the Dart project authors. Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
+// Copyright (c) 2026, the Dart project authors. All rights reserved.
+// Copyright 2026, the Flutter authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
 
 import 'package:jaspr/dom.dart';
 import 'package:jaspr/jaspr.dart';
