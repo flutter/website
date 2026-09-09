240d142e2712ab259d60e58b2a761e87a9838dac
Manuel Santos Souza <manuelssouza23@icloud.com>
2026-09-09 19:09:54 -0300

    Fix stray leading space in blog_toc.dart

--- packages/site_shared/lib/components/blog/blog_toc.dart
+++ packages/site_shared/lib/components/blog/blog_toc.dart
@@ -1,4 +1,4 @@
- // Copyright (c) 2026, the Dart project authors. All rights reserved.
+// Copyright (c) 2026, the Dart project authors. All rights reserved.
 // Copyright 2026, the Flutter authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
